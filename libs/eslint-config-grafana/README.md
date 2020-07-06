b19557cda00451005e4b9ee3ed83632e60b12847
Jack Westbrook <jack.westbrook@gmail.com>
2020-07-06 20:03:57 +0200

    Refactor and and version bumps
    Zoltan/refactor and version bump

--- libs/eslint-config-grafana/README.md
+++ libs/eslint-config-grafana/README.md
@@ -1,19 +1,38 @@
 # grafana-tsconfig
+
 > Grafana's [ESLint](https://eslint.org) config.
 
-
 ## Installation
+
 ```shell
 yarn add @grafana/eslint-config --dev
 ```
 
 or:
+
 ```shell
 npm install @grafana/eslint-config --save-dev
 ```
 
+Don't forget to install peerDependencies.
+
+1. Install the correct versions of each package, which are listed by the command:
+
+```sh
+npm info "@grafana/eslint-config@latest" peerDependencies
+```
+
+If using **npm 5+**, use this shortcut
+
+```sh
+npx install-peerdeps --dev @grafana/eslint-config
+```
+
+If using **yarn**, you can also use the shortcut described above if you have npm 5+ installed on your machine, as the command will detect that you are using yarn and will act accordingly.
+Otherwise, run `npm info "@grafana/eslint-config@latest" peerDependencies` to list the peer dependencies and versions, then run `yarn add --dev <dependency>@<version>` for each listed peer dependency.
 
 ## Usage
+
 ```json
 {
   "extends": ["@grafana/eslint-config"]
@@ -22,14 +41,14 @@ npm install @grafana/eslint-config --save-dev
 
 It will automatically handle `*.(js|ts|tsx)` files.
 
-
 ## Publishing
+
 ```shell
 npm publish
 ```
 
 Also be sure to update any official packages that depend on this with fixes and version increases.
 
-
 ## Versioning
-It's probable that *any* change will be a breaking one, so it's best to stick to major version releases.
+
+It's probable that _any_ change will be a breaking one, so it's best to stick to major version releases.
--- libs/eslint-config-grafana/index.js
+++ libs/eslint-config-grafana/index.js
@@ -1,18 +1,85 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  'overrides': [
-    {
-      'extends': ['./config-js.js'],
-      'files': ['*.js']
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
     },
-    {
-      'extends': ['./config-ts.js'],
-      'files': ['*.ts']
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
-    {
-      'extends': ['./config-tsx.js'],
-      'files': ['*.tsx']
-    }
-  ]
+  },
+  plugins: ["jsdoc", "prettier", "@typescript-eslint", "react-hooks"],
+  extends: ["plugin:react-hooks/recommended", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
+  rules: {
+    curly: "error",
+    "dot-notation": "off",
+    "eol-last": "error",
+    eqeqeq: ["error", "always", { null: "ignore" }],
+    "guard-for-in": "off",
+    "jsdoc/check-alignment": "error",
+    "new-parens": "error",
+    "no-array-constructor": "error",
+    "no-bitwise": "off",
+    "no-caller": "error",
+    "no-cond-assign": "error",
+    "no-console": ["error", { allow: ["error", "log", "warn"] }],
+    "no-debugger": "error",
+    "no-empty": "off",
+    "no-eval": "error",
+    "no-fallthrough": "off",
+    "no-new-wrappers": "error",
+    "no-redeclare": "error",
+    "no-restricted-imports": ["error", "moment"],
+    "no-shadow": "off",
+    "no-unused-expressions": "off",
+    "no-unused-labels": "error",
+    "prettier/prettier": "error",
+    radix: "error",
+    "sort-keys": "off",
+    "spaced-comment": ["off", "always"],
+    "use-isnan": "error",
+
+    "@typescript-eslint/no-unused-expressions": [
+      "error",
+      { allowShortCircuit: true, allowTernary: true },
+    ],
+    "@typescript-eslint/array-type": ["error", { default: "array-simple" }],
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "interface",
+        format: ["PascalCase"],
+        custom: {
+          regex: "^I[A-Z]",
+          match: false,
+        },
+      },
+    ],
+    "@typescript-eslint/consistent-type-assertions": "error",
+    "@typescript-eslint/explicit-member-accessibility": [
+      "error",
+      { accessibility: "no-public" },
+    ],
+    "@typescript-eslint/no-inferrable-types": "error",
+    "@typescript-eslint/no-namespace": ["error", { allowDeclarations: false }],
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/triple-slash-reference": "error",
+    "@typescript-eslint/type-annotation-spacing": [
+      "error",
+      {
+        after: true,
+        before: false,
+        overrides: {
+          arrow: { after: true, before: true },
+        },
+      },
+    ],
+  },
 };
