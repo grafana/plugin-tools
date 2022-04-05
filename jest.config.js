694d08ea59c0dc67635894c47f1605cb5a5388c8
Levente Balogh <balogh.levente.hu@gmail.com>
2022-04-05 12:48:14 +0200

    chore: add a Jest config for the project

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  testMatch: ['<rootDir>/src/**/*.{spec,test,jest}.{js,jsx,ts,tsx}'],
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        sourceMaps: true,
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+            decorators: false,
+            dynamicImport: true,
+          },
+        },
+      },
+    ],
+  },
+  modulePathIgnorePatterns: ['<rootDir>/templates/'],
+};
