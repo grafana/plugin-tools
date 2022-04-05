0949b79a15b96e68ad5b5e96a68c89ad5e1f3049
Levente Balogh <balogh.levente.hu@gmail.com>
2022-04-05 12:46:32 +0200

    feat: add utils for working with templates

---
+++ src/utils/tests/utils.templates.test.ts
@@ -0,0 +1,29 @@
+import { PLUGIN_TYPES } from '../../constants';
+import { getTemplateFiles } from '../utils.templates';
+
+describe('Utils / Templates', () => {
+  describe('getTemplateFiles()', () => {
+    test('should return with a list of files', () => {
+      expect(getTemplateFiles(PLUGIN_TYPES.app).length).toBeGreaterThan(0);
+    });
+
+    test('should be possible to filter templates for a sub-folder', () => {
+      const templateFiles = getTemplateFiles(PLUGIN_TYPES.app, '.config/types');
+
+      expect(templateFiles.length).toBeGreaterThan(0);
+
+      // A file that should live in this sub-directory
+      expect(templateFiles.find((t) => t.includes('custom.d.ts'))).not.toBeUndefined();
+
+      // Something that should not be found in this sub-directory
+      expect(templateFiles.find((t) => t.includes('Dockerfile'))).toBeUndefined();
+    });
+
+    test('should return an empty array when scanning a non-existing sub-folder', () => {
+      const templateFiles = getTemplateFiles(PLUGIN_TYPES.app, '.config/types/foo/bar/unknown');
+
+      expect(Array.isArray(templateFiles)).toBe(true);
+      expect(templateFiles.length).toBe(0);
+    });
+  });
+});
