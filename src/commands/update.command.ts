ce97ffec1fbb4e86a39b9992e9f56ecf84e51f46
Levente Balogh <balogh.levente.hu@gmail.com>
2022-04-13 09:04:21 +0200

    refactor(Command): refactor the `update` command for simplicity

---
+++ src/commands/update.command.ts
@@ -0,0 +1,66 @@
+import { TEXT, UDPATE_CONFIG } from '../constants';
+import { compileTemplateFiles, getTemplateData } from '../utils/utils.templates';
+import { confirmPrompt, selectPrompt, printMessage, printSuccessMessage } from '../utils/utils.console';
+import {
+  updatePackageJson,
+  hasNpmDependenciesToUpdate,
+  getPackageJsonUpdatesAsText,
+  updateNpmScripts,
+} from '../utils/utils.npm';
+
+export const update = async () => {
+  try {
+    // 0. Warning
+    // ----------
+    printMessage(TEXT.updateCommandWarning);
+
+    // 1. Add / update configuration files
+    // -----------------------------------
+    if (await confirmPrompt(TEXT.overrideFilesPrompt)) {
+      compileTemplateFiles(UDPATE_CONFIG.filesToOverride, getTemplateData());
+      printSuccessMessage(TEXT.overrideFilesSuccess);
+    } else {
+      printMessage(TEXT.overrideFilesAborted);
+      process.exit(0);
+    }
+
+    // 2. Add / update dependencies inside the `package.json`
+    // (skipped automatically if there is nothing to update)
+    // ------------------------------------------------
+    if (hasNpmDependenciesToUpdate()) {
+      const PROMPT_CHOICES = {
+        ALL: 'Yes, all of them',
+        ONLY_OUTDATED: 'Yes, but only the outdated ones',
+        NONE: 'No',
+      };
+      const shouldUpdateDeps = await selectPrompt(TEXT.updateNpmDependenciesPrompt + getPackageJsonUpdatesAsText(), [
+        PROMPT_CHOICES.ALL,
+        PROMPT_CHOICES.ONLY_OUTDATED,
+        PROMPT_CHOICES.NONE,
+      ]);
+
+      if (shouldUpdateDeps && shouldUpdateDeps !== PROMPT_CHOICES.NONE) {
+        updatePackageJson({ onlyOutdated: shouldUpdateDeps === PROMPT_CHOICES.ONLY_OUTDATED });
+        printSuccessMessage(TEXT.updateNpmDependenciesSuccess);
+      } else {
+        printMessage(TEXT.updateNpmDependenciesAborted);
+      }
+    }
+
+    // 3. Add / update NPM scripts
+    // (skipped automatically if there is nothing to update)
+    // ------------------------------------------------
+    if (await confirmPrompt(TEXT.updateNpmScriptsPrompt)) {
+      updateNpmScripts();
+      printSuccessMessage(TEXT.updateNpmScriptsSuccess);
+    } else {
+      printMessage(TEXT.updateNpmScriptsAborted);
+    }
+
+    // 4. Summary
+    // -------------
+    printSuccessMessage(TEXT.updateCommandSuccess);
+  } catch (error) {
+    console.error(error);
+  }
+};
