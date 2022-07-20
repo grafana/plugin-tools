ce400ea87b23e5d47a88efe70cd199ef84054dbd
Esteban Beltran <esteban.beltran@elastic.co>
2022-07-20 14:04:14 +0200

    Add organization name and plugin type suffix

---
+++ src/utils/tests/utils.handlebars.test.ts
@@ -0,0 +1,35 @@
+import { PLUGIN_TYPES } from '../../constants';
+import { normalizeId } from '../utils.handlebars';
+
+describe('Handlebars helpers', () => {
+  describe('normalize id', () => {
+    test.each([PLUGIN_TYPES.app, PLUGIN_TYPES.datasource, PLUGIN_TYPES.panel])(
+      'should return the id with the type appended',
+      (type: PLUGIN_TYPES) => {
+        const pluginName = 'my-plugin';
+        const orgName = 'my-org';
+        const actual = normalizeId(pluginName, orgName, type);
+        expect(actual).toEqual(`my-org-my-plugin-${type}`);
+      }
+    );
+    test.each([PLUGIN_TYPES.app, PLUGIN_TYPES.datasource, PLUGIN_TYPES.panel])(
+      'should not duplicate the type if it is already present',
+      (type: PLUGIN_TYPES) => {
+        const pluginName = `my-plugin-${type}`;
+        const orgName = 'my-org';
+        const actual = normalizeId(pluginName, orgName, type);
+        expect(actual).toEqual(`my-org-my-plugin-${type}`);
+      }
+    );
+
+    test.each([PLUGIN_TYPES.app, PLUGIN_TYPES.datasource, PLUGIN_TYPES.panel])(
+      'should not duplicate the type if it is already present (no dash before type)',
+      (type: PLUGIN_TYPES) => {
+        const pluginName = `my-plugin${type}`;
+        const orgName = 'my-org';
+        const actual = normalizeId(pluginName, orgName, type);
+        expect(actual).toEqual(`my-org-my-plugin-${type}`);
+      }
+    );
+  });
+});
