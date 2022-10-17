240e4c35e4a07d804080d43f1d19d123eb4062d3
Jack Westbrook <jack.westbrook@gmail.com>
2022-10-17 19:18:32 +0200

    chore: add sign-plugin workspace, make sure clean and build commands work

---
+++ packages/sign-plugin/src/bin/run.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import minimist from 'minimist';
+import { sign } from '../commands';
+
+const args = process.argv.slice(2);
+const argv = minimist(args);
+const commands: Record<string, (argv: minimist.ParsedArgs) => void> = {
+  sign,
+};
+const command = commands[argv._[0]] || commands.generate;
+
+command(argv);
