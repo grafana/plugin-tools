7d47e49281ce72c3be33b3426ae6095ae8dd7375
Giuseppe Guerra <giuseppe@guerra.in>
2022-11-08 17:31:37 +0100

    Create-Plugin: Add backend-app template

---
+++ packages/create-plugin/templates/backend-app/pkg/plugin/app.go
@@ -0,0 +1,122 @@
+package plugin
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/resource/httpadapter"
+	"github.com/grafana/grafana-plugin-sdk-go/data"
+	"math/rand"
+	"time"
+)
+
+// Make sure App implements required interfaces. This is important to do
+// since otherwise we will only get a not implemented error response from plugin in
+// runtime. In this example app instance implements backend.QueryDataHandler,
+// backend.CheckHealthHandler and backend.CallResourceHandler interfaces.
+// Plugin should not implement all these interfaces - only those which are
+// required for a particular task.
+var (
+	_ backend.CallResourceHandler   = (*App)(nil)
+	_ backend.QueryDataHandler      = (*App)(nil)
+	_ backend.CheckHealthHandler    = (*App)(nil)
+	_ instancemgmt.InstanceDisposer = (*App)(nil)
+)
+
+// App is an example app backend plugin which can respond to data queries, reports
+// its health and handle resource calls.
+type App struct {
+	backend.CallResourceHandler
+}
+
+// NewApp creates a new example *App instance.
+func NewApp(_ backend.AppInstanceSettings) (instancemgmt.Instance, error) {
+	var app App
+
+	// Use a httpadapter (provided by the SDK) for resource calls. This allows us
+	// to use a *http.ServeMux for resource calls, so we can map multiple routes
+	// to CallResource without having to implement extra logic.
+	app.CallResourceHandler = httpadapter.New(newResourceHTTPServeMux(&app))
+
+	return &app, nil
+}
+
+// Dispose here tells plugin SDK that plugin wants to clean up resources when a new instance
+// created. As soon as datasource settings change detected by SDK old datasource instance will
+// be disposed and a new one will be created using the factory function.
+func (a *App) Dispose() {
+	// cleanup
+}
+
+// QueryData handles multiple queries and returns multiple responses.
+// request contains the queries []DataQuery (where each query contains RefID as a unique identifier).
+// The QueryDataResponse contains a map of RefID to the response for each query, and each response
+// contains Frames ([]*Frame).
+func (a *App) QueryData(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
+	log.DefaultLogger.Info("QueryData called", "request", req)
+
+	// create response struct
+	response := backend.NewQueryDataResponse()
+
+	// loop over queries and execute them individually.
+	for _, q := range req.Queries {
+		res := a.query(ctx, req.PluginContext, q)
+
+		// save the response in a hashmap
+		// based on with RefID as identifier
+		response.Responses[q.RefID] = res
+	}
+
+	return response, nil
+}
+
+type queryModel struct{}
+
+func (a *App) query(_ context.Context, pCtx backend.PluginContext, query backend.DataQuery) backend.DataResponse {
+	response := backend.DataResponse{}
+
+	// Unmarshal the JSON into our queryModel.
+	var qm queryModel
+
+	response.Error = json.Unmarshal(query.JSON, &qm)
+	if response.Error != nil {
+		return response
+	}
+
+	// create data frame response.
+	frame := data.NewFrame("response")
+
+	// add fields.
+	frame.Fields = append(frame.Fields,
+		data.NewField("time", nil, []time.Time{query.TimeRange.From, query.TimeRange.To}),
+		data.NewField("values", nil, []int64{10, 20}),
+	)
+
+	// add the frames to the response.
+	response.Frames = append(response.Frames, frame)
+
+	return response
+}
+
+// CheckHealth handles health checks sent from Grafana to the plugin.
+// The main use case for these health checks is the test button on the
+// datasource configuration page which allows users to verify that
+// a datasource is working as expected.
+func (a *App) CheckHealth(_ context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
+	log.DefaultLogger.Info("CheckHealth called", "request", req)
+
+	var status = backend.HealthStatusOk
+	var message = "Data source is working"
+
+	if rand.Int()%2 == 0 {
+		status = backend.HealthStatusError
+		message = "randomized error"
+	}
+
+	return &backend.CheckHealthResult{
+		Status:  status,
+		Message: message,
+	}, nil
+}
---
+++ packages/create-plugin/templates/backend-app/pkg/plugin/resources.go
@@ -0,0 +1,47 @@
+package plugin
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// handlePing is an example HTTP GET resource that returns a {"message": "ok"} JSON response.
+func (a *App) handlePing(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	if _, err := w.Write([]byte(`{"message": "ok"}`)); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+// handleEcho is an example HTTP POST resource that accepts a JSON with a "message" key and
+// returns to the client whatever it is sent.
+func (a *App) handleEcho(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	var body struct {
+		Message string `json:"message"`
+	}
+	if err := json.NewDecoder(req.Body).Decode(&body); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(body); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+// newResourceHTTPServeMux returns a new *http.ServeMux with some pre-registered HTTP handlers.
+// It can be wrapped in a httpadapter and used in CallResource.
+func newResourceHTTPServeMux(app *App) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", app.handlePing)
+	mux.HandleFunc("/echo", app.handleEcho)
+	return mux
+}
