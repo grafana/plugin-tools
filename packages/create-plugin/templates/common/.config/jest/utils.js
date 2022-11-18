f3a94d0359f8ed954063a8e06fcba679f56611b7
Jack Westbrook <jack.westbrook@gmail.com>
2022-11-18 09:31:12 +0100

    fix(create-plugin): prefer cjs require over es exports

--- packages/create-plugin/templates/common/.config/jest/utils.js
+++ packages/create-plugin/templates/common/.config/jest/utils.js
@@ -8,7 +8,12 @@
  * This utility function is useful in combination with jest `transformIgnorePatterns` config
  * to transform specific packages (e.g.ES modules) in a projects node_modules folder.
  */
-export const nodeModulesToTransform = (moduleNames) => `node_modules\/(?!(${moduleNames.join('|')})\/)`;
+const nodeModulesToTransform = (moduleNames) => `node_modules\/(?!(${moduleNames.join('|')})\/)`;
 
 // Array of known nested grafana package dependencies that only bundle an ESM version
-export const grafanaESModules = ['ol', 'react-colorful'];
+const grafanaESModules = ['ol', 'react-colorful'];
+
+module.exports = {
+  nodeModulesToTransform,
+  grafanaESModules
+}
