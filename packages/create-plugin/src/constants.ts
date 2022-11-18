b9964259b3674099675efbcbc5aebfbe2871e019
Jack Westbrook <jack.westbrook@gmail.com>
2022-11-18 11:26:43 +0100

    Create-Plugin: Update grafanaVersion to 9.2.5 (#149)

--- packages/create-plugin/src/constants.ts
+++ packages/create-plugin/src/constants.ts
@@ -38,7 +38,7 @@ export enum PLUGIN_TYPES {
 // and will be available to use in the templates.
 // Example: "@grafana/ui": "{{ grafanaVersion }}"
 export const EXTRA_TEMPLATE_VARIABLES = {
-  grafanaVersion: '9.1.2',
+  grafanaVersion: '9.2.5',
 };
 
 export const GRAFANA_FE_PACKAGES = [
