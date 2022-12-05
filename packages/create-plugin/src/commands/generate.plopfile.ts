6c9765f4356507324295e1ad4524be9d8dafbb18
Sarah Zinger <sarah.zinger@grafana.com>
2022-12-05 15:48:07 -0500

    Update plop file too.

--- packages/create-plugin/src/commands/generate.plopfile.ts
+++ packages/create-plugin/src/commands/generate.plopfile.ts
@@ -252,9 +252,9 @@ function printSuccessMessage(answers: CliArgs) {
   const commands = [
     `- \`cd ./${directory}\``,
     '- `yarn install` to install frontend dependencies.',
-    '- `docker-compose up` to start a grafana development server.',
     '- `yarn dev` to build (and watch) the plugin frontend code.',
-    ...(answers.hasBackend ? ['- `mage -v build:linux` to build the plugin backend code.'] : []),
+    ...(answers.hasBackend ? ['- `mage -v build:linux` to build the plugin backend code. Rerun this command every time you edit your backend files.'] : []),
+    '- `docker-compose up` to start a grafana development server. Restart this command after each time you run mage to run your new backend code.',
     '- Open http://localhost:3000 in your browser to create a dashboard to begin developing your plugin.',
   ];
 
