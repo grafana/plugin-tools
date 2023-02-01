7f8be4c971d0db8e5463ced4b9e84eda30ec3f20
Jack Westbrook <jack.westbrook@gmail.com>
2023-02-01 16:28:44 +0100

    fix(webpack): handle nested plugins regardless of directory depth

--- packages/create-plugin/templates/common/.config/webpack/utils.ts
+++ packages/create-plugin/templates/common/.config/webpack/utils.ts
@@ -20,21 +20,22 @@ export function hasReadme() {
   return fs.existsSync(path.resolve(process.cwd(), SOURCE_DIR, 'README.md'));
 }
 
+// Support bundling nested plugins by finding all plugin.json files in src directory
+// then checking for a sibling module.[jt]sx? file.
 export async function getEntries(): Promise<Record<string, string>> {
   const pluginsJson = await globAsync('**/src/**/plugin.json', { absolute: true });
 
   const plugins = await Promise.all(pluginsJson.map((pluginJson) => {
       const folder = path.dirname(pluginJson);
-      return globAsync(`${folder}/module.{ts,tsx,js}`, { absolute: true });
+      return globAsync(`${folder}/module.{ts,tsx,js,jsx}`, { absolute: true });
     })
   );
 
   return plugins.reduce((result, modules) => {
     return modules.reduce((result, module) => {
       const pluginPath = path.dirname(module);
-      const pluginName = path.basename(pluginPath);
-      // support bundling nested plugins
-      const entryName = pluginName === 'src' ? 'module' : `${pluginName}/module`;
+      const pluginName = path.relative(process.cwd(), pluginPath).replace(/src\/?/i, '');
+      const entryName = pluginName === '' ? 'module' : `${pluginName}/module`;
 
       result[entryName] = module;
       return result;
