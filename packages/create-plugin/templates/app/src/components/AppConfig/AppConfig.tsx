8d11fa1f0f7581ce86eb95b618cdb39934dd57ae
Jack Westbrook <jack.westbrook@gmail.com>
2023-05-11 16:47:23 +0100

    feat(create-plugin): scaffold app plugins with pages that use top nav

--- packages/create-plugin/templates/app/src/components/AppConfig/AppConfig.tsx
+++ packages/create-plugin/templates/app/src/components/AppConfig/AppConfig.tsx
@@ -1,23 +1,62 @@
-import React from 'react';
-import { Button, Legend, useStyles2 } from '@grafana/ui';
-import { PluginConfigPageProps, AppPluginMeta, PluginMeta, GrafanaTheme2 } from '@grafana/data';
-import { getBackendSrv } from '@grafana/runtime';
+import React, { ChangeEvent, useState } from 'react';
 import { css } from '@emotion/css';
-import { lastValueFrom } from 'rxjs';
+import { AppPluginMeta, GrafanaTheme2, PluginConfigPageProps, PluginMeta } from '@grafana/data';
+import { getBackendSrv, locationService } from '@grafana/runtime';
+import { Button, Field, FieldSet, Input, SecretInput, useStyles2 } from '@grafana/ui';
+import { testIds } from '../testIds';
 
-export type AppPluginSettings = {};
+export type JsonData = {
+  apiUrl?: string;
+  isApiKeySet?: boolean;
+};
 
-export interface AppConfigProps extends PluginConfigPageProps<AppPluginMeta<AppPluginSettings>> {}
+type State = {
+  // The URL to reach our custom API.
+  apiUrl: string;
+  // Tells us if the API key secret is set.
+  // Set to `true` ONLY if it has already been set and haven't been changed.
+  // (We unfortunately need an auxiliray variable for this, as `secureJsonData` is never exposed to the browser after it is set)
+  isApiKeySet: boolean;
+  // An secret key for our custom API.
+  apiKey: string;
+};
 
-export const AppConfig = ({ plugin }: AppConfigProps) => {
+interface Props extends PluginConfigPageProps<AppPluginMeta<JsonData>> {}
+
+export const AppConfig = ({ plugin }: Props) => {
   const s = useStyles2(getStyles);
-  const { enabled, jsonData } = plugin.meta;
+  const { enabled, pinned, jsonData } = plugin.meta;
+  const [state, setState] = useState<State>({
+    apiUrl: jsonData?.apiUrl || '',
+    apiKey: '',
+    isApiKeySet: Boolean(jsonData?.isApiKeySet),
+  });
+
+  const onResetApiKey = () =>
+    setState({
+      ...state,
+      apiKey: '',
+      isApiKeySet: false,
+    });
+
+  const onChangeApiKey = (event: ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      apiKey: event.target.value.trim(),
+    });
+  };
+
+  const onChangeApiUrl = (event: ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      apiUrl: event.target.value.trim(),
+    });
+  };
 
   return (
-    <div className="gf-form-group">
-      <div>
-        {/* Enable the plugin */}
-        <Legend>Enable / Disable</Legend>
+    <div data-testid={testIds.appConfig.container}>
+      {/* ENABLE / DISABLE PLUGIN */}
+      <FieldSet label="Enable / Disable">
         {!enabled && (
           <>
             <div className={s.colorWeak}>The plugin is currently not enabled.</div>
@@ -56,7 +95,64 @@ export const AppConfig = ({ plugin }: AppConfigProps) => {
             </Button>
           </>
         )}
-      </div>
+      </FieldSet>
+
+      {/* CUSTOM SETTINGS */}
+      <FieldSet label="API Settings" className={s.marginTopXl}>
+        {/* API Key */}
+        <Field label="API Key" description="A secret key for authenticating to our custom API">
+          <SecretInput
+            width={60}
+            data-testid={testIds.appConfig.apiKey}
+            id="api-key"
+            value={state?.apiKey}
+            isConfigured={state.isApiKeySet}
+            placeholder={'Your secret API key'}
+            onChange={onChangeApiKey}
+            onReset={onResetApiKey}
+          />
+        </Field>
+
+        {/* API Url */}
+        <Field label="API Url" description="" className={s.marginTop}>
+          <Input
+            width={60}
+            id="api-url"
+            data-testid={testIds.appConfig.apiUrl}
+            label={`API Url`}
+            value={state?.apiUrl}
+            placeholder={`E.g.: http://mywebsite.com/api/v1`}
+            onChange={onChangeApiUrl}
+          />
+        </Field>
+
+        <div className={s.marginTop}>
+          <Button
+            type="submit"
+            data-testid={testIds.appConfig.submit}
+            onClick={() =>
+              updatePluginAndReload(plugin.meta.id, {
+                enabled,
+                pinned,
+                jsonData: {
+                  apiUrl: state.apiUrl,
+                  isApiKeySet: true,
+                },
+                // This cannot be queried later by the frontend.
+                // We don't want to override it in case it was set previously and left untouched now.
+                secureJsonData: state.isApiKeySet
+                  ? undefined
+                  : {
+                      apiKey: state.apiKey,
+                    },
+              })
+            }
+            disabled={Boolean(!state.apiUrl || (!state.isApiKeySet && !state.apiKey))}
+          >
+            Save API settings
+          </Button>
+        </div>
+      </FieldSet>
     </div>
   );
 };
@@ -68,25 +164,29 @@ const getStyles = (theme: GrafanaTheme2) => ({
   marginTop: css`
     margin-top: ${theme.spacing(3)};
   `,
+  marginTopXl: css`
+    margin-top: ${theme.spacing(6)};
+  `,
 });
 
-const updatePluginAndReload = async (pluginId: string, data: Partial<PluginMeta>) => {
+const updatePluginAndReload = async (pluginId: string, data: Partial<PluginMeta<JsonData>>) => {
   try {
     await updatePlugin(pluginId, data);
 
     // Reloading the page as the changes made here wouldn't be propagated to the actual plugin otherwise.
     // This is not ideal, however unfortunately currently there is no supported way for updating the plugin state.
-    window.location.reload();
+    locationService.reload();
   } catch (e) {
     console.error('Error while updating the plugin', e);
   }
 };
 
 export const updatePlugin = async (pluginId: string, data: Partial<PluginMeta>) => {
-  const response = getBackendSrv().fetch({
+  const response = await getBackendSrv().datasourceRequest({
     url: `/api/plugins/${pluginId}/settings`,
     method: 'POST',
     data,
   });
-  return lastValueFrom(response);
+
+  return response?.data;
 };
