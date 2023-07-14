5eeea8cd8e77030da1bbc30a86a78f5edd0a4f89
Timur Olzhabayev <timur.olzhabayev@grafana.com>
2023-07-14 12:19:51 +0100

    Adding noindex for now until we reshuffle the content (#305)

--- docusaurus/website/docusaurus.config.js
+++ docusaurus/website/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.png',
-
+  noIndex: true,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'grafana', // Usually your GitHub org/user name.
