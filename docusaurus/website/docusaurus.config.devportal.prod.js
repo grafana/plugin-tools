0075a55978c2e9918f1955a867b92ab10b933636
Timur Olzhabayev <timur.olzhabayev@grafana.com>
2023-08-11 15:28:29 +0200

    prod deployment (#345)

---
+++ docusaurus/website/docusaurus.config.devportal.prod.js
@@ -0,0 +1,67 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const {
+  generalConfig,
+  plugins,
+  presetsDocs,
+  presetsTheme,
+  themeConfigNavbar,
+  themeConfigFooter,
+  themeConfigPrism,
+  themeConfigColorMode,
+} = require('./docusaurus.config.base');
+
+
+// Replace docs link in footer so builds don't have broken links.
+const [docs, ...rest] = themeConfigFooter.links;
+const footerConfig = {
+  ...themeConfigFooter,
+  links: [
+    {
+      title: 'Docs',
+      items: [
+        {
+          label: 'Get Started',
+          to: '/',
+        },
+      ],
+    },
+    ...rest,
+  ],
+};
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  ...generalConfig,
+  url: 'https://grafana.com/',
+  baseUrl: 'developers/plugin-tools/',
+
+  plugins: plugins,
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          ...presetsDocs,
+          routeBasePath: '/',
+        },
+        theme: presetsTheme,
+        blog: false,
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: themeConfigNavbar,
+      footer: footerConfig,
+      prism: themeConfigPrism,
+      colorMode: themeConfigColorMode,
+    }),
+};
+
+module.exports = config;
