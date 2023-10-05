5e7b03e13a649be74edf6bdc1508becd97fdea63
Jack Westbrook <jack.westbrook@gmail.com>
2023-10-05 09:18:11 +0200

    Wip

--- .github/actions/check-labels/index.js
+++ .github/actions/check-labels/index.js
@@ -1,16 +1,57 @@
+// @ts-check
 const core = require('@actions/core');
-const { context } = require('@actions/github');
+const { context, getOctokit } = require('@actions/github');
 
-function run() {
+async function run() {
   try {
-    const pr = context.payload.pull_request || {};
-    const labels = pr.labels || [];
+    const {
+      payload: { pull_request },
+      repo,
+    } = context;
+    const labels = pull_request?.labels || [];
     const labelNames = labels.map((label) => label.name);
+    const githubToken = core.getInput('github_token') || process.env.GITHUB_TOKEN;
+    const prNumber = pull_request?.number;
+
+    if (!Boolean(githubToken)) {
+      core.setFailed('');
+      return;
+    }
 
     const requiredOneOfLabels = ['patch', 'minor', 'major', 'skip-changelog'];
-
     const hasSemverLabel = labelNames.filter((label) => requiredOneOfLabels.includes(label));
     const hasReleaseLabel = labelNames.includes('release');
+    const prMessageSymbol = `<!-- plugin-tools-auto-check-labels-comment -->`;
+    const prMessage = `👋 This repository uses [Auto](https://intuit.github.io/auto/) for releasing packages using PR labels.`;
+    const prMessageIssues = [];
+
+    console.log({
+      labels,
+      labelNames,
+      prNumber,
+    });
+
+    const octokit = getOctokit(githubToken);
+
+    const { data } = await octokit.rest.issues.listComments({
+      ...repo,
+      issue_number: prNumber,
+    });
+
+    let previousCommentId;
+    for (const { body, id } of data) {
+      if (body?.includes(prMessageSymbol)) {
+        previousCommentId = id;
+      }
+    }
+
+    console.log({ previousCommentId });
+
+    // await octokit.rest.issues.createComment({
+    //   ...repo,
+    //   issue_number: prNumber,
+    //   body: prMessage,
+    // });
 
     if (hasSemverLabel.length === 0) {
       core.setFailed('PR is missing one of the following labels: `patch`, `minor`, `major`, `skip-changelog`');
@@ -24,11 +65,12 @@ function run() {
 
     if (hasSemverLabel.length === 1 && !hasReleaseLabel) {
       core.setOutput(
-        'PR has required semver label but missing `release` label. This PR can be merged but will not publish a release.'
+        'canMergeWithoutPublish',
+        'PR has required semver label but missing `release` label. This PR can be merged but will not trigger a new release.'
       );
     }
 
-    core.setOutput('PR will create a new `${hasSemverLabel}` release.');
+    core.setOutput('willPublish', 'PR will trigger a new `${hasSemverLabel}` release.');
   } catch (error) {
     core.setFailed(error.message);
   }
