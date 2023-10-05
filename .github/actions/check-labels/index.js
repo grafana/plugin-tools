4f22c68f09004c1ad132c44a36af69c99968f787
Jack Westbrook <jack.westbrook@gmail.com>
2023-10-05 10:14:14 +0200

    Wip

--- .github/actions/check-labels/index.js
+++ .github/actions/check-labels/index.js
@@ -14,17 +14,10 @@ async function run() {
     const prNumber = pull_request?.number;
 
     const requiredOneOfLabels = ['patch', 'minor', 'major', 'skip-changelog'];
-    const hasSemverLabel = labelNames.filter((label) => requiredOneOfLabels.includes(label));
+    const attachedSemverLabels = labelNames.filter((label) => requiredOneOfLabels.includes(label));
     const hasReleaseLabel = labelNames.includes('release');
     const prMessageSymbol = `<!-- plugin-tools-auto-check-labels-comment -->`;
-    const prMessage = `👋 This repository uses [Auto](https://intuit.github.io/auto/) for releasing packages using PR labels.`;
-    const prMessageIssues = [];
-
-    console.log({
-      labels,
-      labelNames,
-      prNumber,
-    });
+    const prIntroMessage = `👋 This repository uses [Auto](https://intuit.github.io/auto/) for releasing packages using PR labels.`;
 
     const octokit = getOctokit(githubToken);
 
@@ -40,35 +33,56 @@ async function run() {
       }
     }
 
-    console.log({ previousCommentId });
-
-    // await octokit.rest.issues.createComment({
-    //   ...repo,
-    //   issue_number: prNumber,
-    //   body: prMessage,
-    // });
-
-    if (hasSemverLabel.length === 0) {
-      core.setFailed('PR is missing one of the following labels: `patch`, `minor`, `major`, `skip-changelog`');
+    if (attachedSemverLabels.length === 0) {
+      const error = 'This PR is missing one of the following labels: `patch`, `minor`, `major`, `skip-changelog`';
+      const message = `${prMessageSymbol}\n${prIntroMessage}\n\n${error}`;
+      console.log({ previousCommentId, message, ...repo, prNumber });
+      // await doComment({ octokit, previousCommentId, message, repo, prNumber });
+      core.setFailed(error);
     }
 
-    if (hasSemverLabel.length > 1) {
-      core.setFailed(
-        'PR contains multiple semver labels. A PR can only include one of: `patch`, `minor`, `major`, `skip-changelog` labels.'
-      );
+    if (attachedSemverLabels.length > 1) {
+      const error =
+        'This PR contains multiple semver labels. A PR can only include one of: `patch`, `minor`, `major`, `skip-changelog` labels.';
+      const message = `${prMessageSymbol}\n${prIntroMessage}\n\n${error}`;
+      console.log({ previousCommentId, message, ...repo, prNumber });
+      // await doComment({ octokit, previousCommentId, message, repo, prNumber });
+      core.setFailed(error);
     }
 
-    if (hasSemverLabel.length === 1 && !hasReleaseLabel) {
-      core.setOutput(
-        'canMergeWithoutPublish',
-        'PR has required semver label but missing `release` label. This PR can be merged but will not trigger a new release.'
-      );
+    if (attachedSemverLabels.length === 1 && !hasReleaseLabel) {
+      const warning =
+        'This PR has required semver label `${attachedSemverLabels}[0]` but missing `release` label. This PR can be merged but will not trigger new releases.';
+      const message = `${prMessageSymbol}\n${prIntroMessage}\n\n${warning}`;
+      console.log({ previousCommentId, message, ...repo, prNumber });
+      // await doComment({ octokit, previousCommentId, message, repo, prNumber });
+      core.setOutput('canMergeWithoutPublish', warning);
     }
 
-    core.setOutput('willPublish', 'PR will trigger a new `${hasSemverLabel}` release.');
+    core.setOutput('willPublish', 'This PR will trigger a new `${hasSemverLabel}` release when merged.');
   } catch (error) {
     core.setFailed(error.message);
   }
 }
+
+// async function doComment({ octokit, previousCommentId, message, repo, prNumber }) {
+//   try {
+//     if (previousCommentId) {
+//       await octokit.rest.issues.updateComment({
+//         ...repo,
+//         comment_id: previousCommentId,
+//         body: message,
+//       });
+//     } else {
+//       await octokit.rest.issues.createComment({
+//         ...repo,
+//         issue_number: prNumber,
+//         body: message,
+//       });
+//     }
+//   } catch (error) {
+//     core.setFailed(error.message);
+//   }
+// }
 
 run();
