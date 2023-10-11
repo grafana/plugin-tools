abe8c8c9c7f32844efb274ef1b5eda1ec42c3697
Jack Westbrook <jack.westbrook@gmail.com>
2023-10-11 17:28:38 +0200

    Wip

--- .github/actions/check-labels/index.js
+++ .github/actions/check-labels/index.js
@@ -2,7 +2,19 @@
 const core = require('@actions/core');
 const { context, getOctokit } = require('@actions/github');
 
-const { prMessageSymbol, prIntroMessage, prMessageLabelDetails } = require('./constants');
+const prMessageSymbol = `<!-- plugin-tools-auto-check-labels-comment -->`;
+const prIntroMessage = `Hello! 👋 This repository uses [Auto](https://intuit.github.io/auto/) for releasing packages using PR labels.`;
+const prMessageLabelDetails = `<details><summary>🏷️ More info about which labels to use</summary>
+<br />
+
+- If the changes only affect the docs website, documentation, or this repository's tooling add the \`no-changelog\` label.
+- If there are changes to any of the npm packages src files please choose from one of the following labels:
+- 🐛 if this PR fixes a bug add the \`patch\` label
+- 🚀 if this PR includes an enhancement add the \`minor\` label
+- 💥 if this PR includes a breaking change add the \`major\` label
+- Optionally, if you would like this PR to publish new versions of packages when it is merged add the \`release\` label.
+</details>
+`;
 
 async function run() {
   try {
