86a90cfb8c6df4d4979c7aaa85e36e0dd77de3bb
Erik Sundell <erik.sundell87@gmail.com>
2023-11-09 09:48:27 +0100

    add rollup config

---
+++ packages/plugin-e2e/rollup.config.ts
@@ -0,0 +1,35 @@
+import resolve from '@rollup/plugin-node-resolve';
+import path from 'path';
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+import { externals } from 'rollup-plugin-node-externals';
+
+const pkg = require('./package.json');
+
+export default [
+  {
+    input: 'src/index.ts',
+    plugins: [externals({ deps: true, peerDeps: true, packagePath: './package.json' }), resolve(), esbuild()],
+    output: [
+      {
+        format: 'cjs',
+        sourcemap: true,
+        dir: path.dirname(pkg.main),
+      },
+      {
+        format: 'esm',
+        sourcemap: true,
+        dir: path.dirname(pkg.module),
+        preserveModules: true,
+      },
+    ],
+  },
+  {
+    input: './compiled/index.d.ts',
+    plugins: [dts()],
+    output: {
+      file: pkg.types,
+      format: 'es',
+    },
+  },
+];
