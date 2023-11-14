f4c673b64754efe1aedd8f058b73402ad88e4c25
Erik Sundell <erik.sundell87@gmail.com>
2023-11-14 08:12:41 +0100

    change name of resource selector

--- packages/plugin-e2e/src/e2e-selectors/versioned/apis.ts
+++ packages/plugin-e2e/src/e2e-selectors/versioned/apis.ts
@@ -2,7 +2,7 @@ import { MIN_GRAFANA_VERSION } from './constants';
 
 export const versionedAPIs = {
   DataSource: {
-    getResource: {
+    resource: {
       '9.4.4': '/api/datasources/uid/*/resources',
       [MIN_GRAFANA_VERSION]: '/api/datasources/*/resources',
     },
