b68ea82891f54efa94169bbe9ee25d12be898d82
Erik Sundell <erik.sundell87@gmail.com>
2023-11-14 08:11:48 +0100

    add query url to apis

--- packages/plugin-e2e/src/e2e-selectors/versioned/apis.ts
+++ packages/plugin-e2e/src/e2e-selectors/versioned/apis.ts
@@ -6,5 +6,8 @@ export const versionedAPIs = {
       '9.4.4': '/api/datasources/uid/*/resources',
       [MIN_GRAFANA_VERSION]: '/api/datasources/*/resources',
     },
+    query: {
+      [MIN_GRAFANA_VERSION]: '*/**/api/ds/query*',
+    },
   },
 };
