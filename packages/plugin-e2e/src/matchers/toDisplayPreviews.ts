eb2c2d248d913c002ba8a83fb8a84576b0df1f4d
Erik Sundell <erik.sundell87@gmail.com>
2023-11-22 10:47:25 +0100

    add custom matcher for asserting on variable previews

---
+++ packages/plugin-e2e/src/matchers/toDisplayPreviews.ts
@@ -0,0 +1,29 @@
+import { expect } from '@playwright/test';
+import { getMessage } from './utils';
+import { VariableEditPage } from '../models';
+
+const toDisplayPreviews = async (variableEditPage: VariableEditPage, previewTexts: string[]) => {
+  let pass = false;
+  let actual;
+  let message: any = `To find preview of values: ${previewTexts.join(', ')}}`;
+
+  try {
+    await expect(
+      variableEditPage.getByTestIdOrAriaLabel(
+        variableEditPage.ctx.selectors.pages.Dashboard.Settings.Variables.Edit.General.previewOfValuesOption
+      )
+    ).toContainText(previewTexts);
+    return {
+      pass: true,
+      actual: false,
+    };
+  } catch (err: unknown) {
+    return {
+      message: () => getMessage(message, err instanceof Error ? err.toString() : 'Unknown error'),
+      pass,
+      actual,
+    };
+  }
+};
+
+export default toDisplayPreviews;
