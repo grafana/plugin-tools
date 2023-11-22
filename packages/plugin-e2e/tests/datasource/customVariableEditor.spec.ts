b080fc41309578d0b7855e24b55b2f8ad12e2010
Erik Sundell <erik.sundell87@gmail.com>
2023-11-22 14:13:58 +0100

    replace cw tests with redshift

--- packages/plugin-e2e/tests/datasource/customVariableEditor.spec.ts
+++ packages/plugin-e2e/tests/datasource/customVariableEditor.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '../../src';
 import { ProvisionFile } from '../../src/types';
-import { CLOUDWATCH_NAMESPACES, CLOUDWATCH_REGIONS } from './mocks/resource';
+import { REDSHIFT_SCHEMAS, REDSHIFT_TABLES } from './mocks/resource';
 
 const toMetricFindOption = (text: string) => ({
   text,
@@ -13,18 +13,16 @@ test('query type `Metrics` should load regions and namespaces and display them i
   page,
   readProvision,
 }) => {
-  await variableEditPage.mockResourceResponse('regions', CLOUDWATCH_REGIONS.map(toMetricFindOption));
-  await variableEditPage.mockResourceResponse('namespaces', CLOUDWATCH_NAMESPACES.map(toMetricFindOption));
-  const cloudwatchDataSource = await readProvision<ProvisionFile>({
-    filePath: 'datasources/cloudwatch.yaml',
-  }).then((provision) => provision.datasources?.[0]!);
+  await variableEditPage.mockResourceResponse('schemas', REDSHIFT_SCHEMAS);
+  await variableEditPage.mockResourceResponse('tables', REDSHIFT_TABLES);
+  const ds = await readProvision<ProvisionFile>({ filePath: 'datasources/redshift.yaml' }).then(
+    (provision) => provision.datasources?.[0]!
+  );
   await variableEditPage.setVariableType('Query');
-  await variableEditPage.datasource.set(cloudwatchDataSource.name!);
-  await page.getByLabel('Query type').fill('Metrics');
-  await page.keyboard.press('Enter');
-  await page.getByLabel('Region').click();
-  await expect(variableEditPage.getByTestIdOrAriaLabel('Select option')).toContainText(CLOUDWATCH_REGIONS);
+  await variableEditPage.datasource.set(ds.name!);
+  await page.getByLabel('Schema').click();
+  await expect(variableEditPage.getByTestIdOrAriaLabel('Select option')).toContainText(REDSHIFT_SCHEMAS);
   await page.keyboard.press('Escape');
-  await page.getByLabel('Namespace').click();
-  await expect(variableEditPage.getByTestIdOrAriaLabel('Select option')).toContainText(CLOUDWATCH_NAMESPACES);
+  await page.getByLabel('Table').click();
+  await expect(variableEditPage.getByTestIdOrAriaLabel('Select option')).toContainText(REDSHIFT_TABLES);
 });
--- packages/plugin-e2e/tests/datasource/customVariableQueryRunner.integration.spec.ts
+++ packages/plugin-e2e/tests/datasource/customVariableQueryRunner.integration.spec.ts
@@ -1,14 +1,15 @@
 import { expect, test } from '../../src';
 import { ProvisionFile } from '../../src/types';
 
-test('custom variable editor query runner', async ({ variableEditPage, page, readProvision }) => {
-  const cloudwatchDataSource = await readProvision<ProvisionFile>({
-    filePath: 'datasources/cloudwatch.yaml',
-  }).then((provision) => provision.datasources?.[0]!);
+test('custom variable editor query runner', async ({ variableEditPage, page, readProvision, selectors }) => {
+  const ds = await readProvision<ProvisionFile>({ filePath: 'datasources/redshift.yaml' }).then(
+    (provision) => provision.datasources?.[0]!
+  );
   await variableEditPage.setVariableType('Query');
-  await variableEditPage.datasource.set(cloudwatchDataSource.name!);
-  await page.getByLabel('Query type').click();
-  await page.getByText('Namespaces').click();
+  await variableEditPage.datasource.set(ds.name!);
+  await page.waitForFunction(() => (window as any).monaco);
+  await variableEditPage.getByTestIdOrAriaLabel(selectors.components.CodeEditor.container).click();
+  await page.keyboard.insertText('select distinct(environment) from long_format_example');
   await variableEditPage.runQuery();
-  await expect(variableEditPage).toDisplayPreviews(['AWS/ACMPrivateCA', /AWS\/Back.*/]);
+  await expect(variableEditPage).toDisplayPreviews([/stag.*/, 'test']);
 });
