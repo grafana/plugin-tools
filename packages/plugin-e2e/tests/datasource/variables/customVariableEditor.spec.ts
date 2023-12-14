5b0a61bdfcc14e9484eb9c69b53a758aebe39812
Erik Sundell <erik.sundell87@gmail.com>
2023-12-14 14:26:48 +0100

    use enter as esc moves to dashboard start page

--- packages/plugin-e2e/tests/datasource/variables/customVariableEditor.spec.ts
+++ packages/plugin-e2e/tests/datasource/variables/customVariableEditor.spec.ts
@@ -20,7 +20,7 @@ test('should load resources and display them as options when clicking on an inpu
   await variableEditPage.datasource.set(provision.datasources?.[0]!.name!);
   await page.getByLabel('Schema').click();
   await expect(variableEditPage.getByTestIdOrAriaLabel('Select option')).toContainText(REDSHIFT_SCHEMAS);
-  await page.keyboard.press('Escape');
+  await page.keyboard.press('Enter');
   await page.getByLabel('Table').click();
   await expect(variableEditPage.getByTestIdOrAriaLabel('Select option')).toContainText(REDSHIFT_TABLES);
 });
