5c464c2961a2278f3606d3f4f0adfb1aa8508fd0
Erik Sundell <erik.sundell87@gmail.com>
2023-12-21 21:26:51 +0100

    refactor fix for radio buttons

--- packages/plugin-e2e/tests/panel/panel.spec.ts
+++ packages/plugin-e2e/tests/panel/panel.spec.ts
@@ -1,13 +1,14 @@
 import fs from 'fs';
 import { DashboardPage, PanelEditPage, expect, test } from '../../src';
 import { Dashboard } from '../../src/types';
+import { clickRadioButton } from '../utils';
 
 test.describe.configure({ mode: 'parallel' });
 test('add a clock panel in new dashboard and set time format to "12 hour"', async ({ panelEditPage, page }) => {
   await panelEditPage.setVisualization('Clock');
   await panelEditPage.setPanelTitle('Clock panel test');
   await panelEditPage.collapseSection('Clock');
-  await page.getByText('12 Hour').locator('..').click();
+  await clickRadioButton(page, '12 Hour');
   //TODO: add data-testid selector to clock panel
   await expect(page.getByRole('heading', { name: /.*[APap][mM]$/ })).toBeVisible();
 });
@@ -25,6 +26,6 @@ test('open a clock panel in a provisioned dashboard and set time format to "12 h
   await panelEditPage.goto();
   await expect(panelEditPage.getVisualizationName()).toHaveText('Clock');
   await panelEditPage.collapseSection('Clock');
-  await page.getByText('12 Hour').locator('..').click();
+  await clickRadioButton(page, '12 Hour');
   await expect(page.getByRole('heading', { name: /.*[APap][mM]$/ })).toBeVisible();
 });
