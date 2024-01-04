6573437f4775f6252b7f930aa81ce5bc031c7eab
Erik Sundell <erik.sundell87@gmail.com>
2024-01-04 09:54:34 +0100

    wip

--- packages/plugin-e2e/global.d.ts
+++ packages/plugin-e2e/global.d.ts
@@ -1,18 +1,19 @@
-import { PanelError } from './src/types';
+// import { Response } from '@playwright/test';
+// import { PanelError } from './src/types';
 
-export {};
+// export {};
 
-declare global {
-  namespace PlaywrightTest {
-    const r: unique symbol;
-    const t: unique symbol;
-    interface Matchers<R, T> {
-      [r]: R;
-      [t]: T;
-      /**
-       * Asserts that the number of panel errors displayed on the page is equal to 1.
-       */
-      toHavePanelError(this: Matchers<unknown, PanelError>): R;
-    }
-  }
-}
+// declare global {
+//   namespace PlaywrightTest {
+//     const r: unique symbol;
+//     const t: unique symbol;
+//     interface Matchers<R, T> {
+//       [r]: R;
+//       [t]: T;
+//       /**
+//        * Asserts that the number of panel errors displayed on the page is equal to 1.
+//        */
+//       toHavePanelError(this: Matchers<unknown, PanelError>): R;
+//     }
+//   }
+// }
--- packages/plugin-e2e/src/index.ts
+++ packages/plugin-e2e/src/index.ts
@@ -1,4 +1,25 @@
+import { PanelError } from './types';
+
 export { expect, test, type PluginFixture, type PluginOptions } from './api';
 export * from './e2e-selectors';
 export * from './fixtures';
 export * from './models';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace PlaywrightTest {
+    // @ts-ignore
+    const r: unique symbol;
+    // @ts-ignore
+    const t: unique symbol;
+    interface Matchers<R, T> {
+      [r]: R;
+      [t]: T;
+      /**
+       * Asserts that the number of panel errors displayed on the page is equal to 1.
+       */
+      toHavePanelError(this: Matchers<unknown, PanelError>): R;
+      toBeOK(this: Matchers<unknown, Promise<Response>>): R;
+    }
+  }
+}
