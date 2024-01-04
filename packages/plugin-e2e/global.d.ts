3c7dfd3c1c1900dac23f0d29c5ca42cf897ee3a0
Erik Sundell <erik.sundell87@gmail.com>
2024-01-04 09:36:48 +0100

    include globals

---
+++ packages/plugin-e2e/global.d.ts
@@ -0,0 +1,18 @@
+import { PanelError } from './src/types';
+
+export {};
+
+declare global {
+  namespace PlaywrightTest {
+    const r: unique symbol;
+    const t: unique symbol;
+    interface Matchers<R, T> {
+      [r]: R;
+      [t]: T;
+      /**
+       * Asserts that the number of panel errors displayed on the page is equal to 1.
+       */
+      toHavePanelError(this: Matchers<unknown, PanelError>): R;
+    }
+  }
+}
