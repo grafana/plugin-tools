f912aeb6d06a6ba1fbf91db9fbb3e3b886456ddb
Erik Sundell <erik.sundell87@gmail.com>
2024-01-05 09:31:31 +0100

    Plugin E2E: Tidy up selectors (#641)

--- packages/plugin-e2e/src/e2e-selectors/resolver.test.ts
+++ packages/plugin-e2e/src/e2e-selectors/resolver.test.ts
@@ -4,7 +4,6 @@ import { versionedAPIs } from './versioned/apis';
 import { MIN_GRAFANA_VERSION } from './versioned/constants';
 import { VersionedSelectors } from './versioned/types';
 
-const GRAFANA_VERSION = '10.2.0';
 let versionedSelectors: VersionedSelectors = {
   components: versionedComponents,
   pages: versionedPages,
@@ -14,12 +13,6 @@ const originalVersionedSelectors = versionedSelectors;
 describe('resolveSelectors', () => {
   afterEach(() => {
     versionedSelectors = originalVersionedSelectors;
-  });
-
-  test('handles selector without version', () => {
-    versionedSelectors.components.PanelEditor.General.content = 'Panel editor content';
-    const selectors = resolveSelectors(versionedSelectors, GRAFANA_VERSION);
-    expect(selectors.components.PanelEditor.General.content).toBe('Panel editor content');
   });
 
   test('returns the right selector value when it has multiple versions', () => {
