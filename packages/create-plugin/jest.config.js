63c7aeedfb17d558a99ba8276cd4526f6349aee9
Jack Westbrook <jack.westbrook@gmail.com>
2024-01-09 12:40:04 +0100

    test(create-plugin): add esmodule ignore patterns for tests to run

--- packages/create-plugin/jest.config.js
+++ packages/create-plugin/jest.config.js
@@ -1,5 +1,8 @@
 const sharedConfig = require('../../jest.config.base');
+const esModules = ['change-case', 'title-case'].join('|');
+
 module.exports = {
   ...sharedConfig,
   modulePathIgnorePatterns: ['<rootDir>/templates/'],
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
 };
