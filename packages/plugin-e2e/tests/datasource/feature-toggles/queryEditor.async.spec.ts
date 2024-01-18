70356bfb6851001ba80a8145b4f0f660bc3fee94
Erik Sundell <erik.sundell87@gmail.com>
2024-01-18 08:44:06 +0100

    fix test

--- packages/plugin-e2e/tests/datasource/feature-toggles/queryEditor.async.spec.ts
+++ packages/plugin-e2e/tests/datasource/feature-toggles/queryEditor.async.spec.ts
@@ -1,25 +1,30 @@
 import { expect, test } from '../../../src';
 import { ProvisionFile } from '../../../src/types';
 
-test('standard query data handler should only be called once', async ({
+// override the feature toggles defined in playwright.config.ts only for tests in this file
+test.use({
+  featureToggles: {
+    redshiftAsyncQueryDataSupport: true,
+  },
+});
+
+test('async query data handler should return a `finished` status', async ({
+  selectors,
   panelEditPage,
   page,
-  selectors,
   readProvision,
 }) => {
-  const requestListener = (request) => request.url().includes(selectors.apis.DataSource.query) && calledTimes++;
-
   const provisionFile = await readProvision<ProvisionFile>({ filePath: 'datasources/redshift.yaml' });
   await panelEditPage.datasource.set(provisionFile.datasources[0].name!);
   await panelEditPage.timeRange.set({ from: '2020-01-31', to: '2020-02-20' });
   await page.waitForFunction(() => (window as any).monaco);
   await panelEditPage.getByTestIdOrAriaLabel(selectors.components.CodeEditor.container).click();
-
   await page.keyboard.insertText('select * from long_format_example limit 100');
-  let calledTimes = 0;
-  page.on('request', requestListener);
-  await expect(panelEditPage.refreshPanel()).toBeOK();
-  await page.waitForTimeout(2000);
-  await expect(calledTimes).toBe(1);
-  page.off('request', requestListener);
+  await expect(
+    panelEditPage.refreshPanel({
+      waitForResponsePredicateCallback: (r) =>
+        r.url().includes(selectors.apis.DataSource.query) &&
+        r.body().then((body) => body.includes(`"status":"finished"`)),
+    })
+  ).toBeOK();
 });
