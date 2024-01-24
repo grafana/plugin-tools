f97a4af609f8d9b4fd498d88f6811f26725e4ca8
Erik Sundell <erik.sundell87@gmail.com>
2024-01-24 12:48:36 +0100

    add feature toggles

--- packages/plugin-e2e/src/options/index.ts
+++ packages/plugin-e2e/src/options/index.ts
@@ -3,6 +3,7 @@ import { Fixtures } from '@playwright/test';
 import { PluginOptions } from '../api';
 
 const options: Fixtures<{}, PluginOptions> = {
+  featureToggles: [{}, { option: true, scope: 'worker' }],
   provisioningRootDir: [path.join(process.cwd(), 'provisioning'), { option: true, scope: 'worker' }],
 };
 
