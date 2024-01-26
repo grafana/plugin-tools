ea6f87b4dae26445a6416d96d95a3101e2bafe83
Erik Sundell <erik.sundell87@gmail.com>
2024-01-26 08:51:03 +0100

    add helper methods for interacting with the grafana ui

---
+++ packages/plugin-e2e/src/models/uiHelpers.ts
@@ -0,0 +1,35 @@
+import * as semver from 'semver';
+import { PluginTestCtx } from '../types';
+import { GrafanaPage } from './GrafanaPage';
+import { Locator } from '@playwright/test';
+
+/**
+ * Provides helpers for interacting with components on the Grafana UI. Should be used
+ * only for components that are hard to interact with or requires different logic for
+ * different Grafana versions.
+ */
+export function ui(ctx: PluginTestCtx, getByTestIdOrAriaLabel: GrafanaPage['getByTestIdOrAriaLabel']) {
+  return {
+    radioButton: {
+      click: async (label: string, options?: { exact?: boolean }) => {
+        // cannot select by label since this pr https://github.com/grafana/grafana/pull/78010
+        if (semver.gte(ctx.grafanaVersion, '9.3.0')) {
+          await ctx.page.getByText(label, options).click();
+        } else {
+          await ctx.page.getByLabel(label, options).click({ timeout: 1000 });
+        }
+      },
+    },
+    codeEditor: {
+      insertText: async (text: string, options?: { root?: Locator }) => {
+        await ctx.page.waitForFunction(() => (window as any).monaco);
+        await getByTestIdOrAriaLabel(ctx.selectors.components.CodeEditor.container, options?.root).click();
+        await ctx.page.keyboard.insertText(text);
+      },
+      click: async (options?: { root?: Locator }) => {
+        await ctx.page.waitForFunction(() => (window as any).monaco);
+        await getByTestIdOrAriaLabel(ctx.selectors.components.CodeEditor.container, options?.root).click();
+      },
+    },
+  };
+}
