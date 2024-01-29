4654ddbb8ddc2a51f541e222b899f181507f74a5
Levente Balogh <balogh.levente.hu@gmail.com>
2024-01-29 13:54:09 +0100

    feat: add functions for checking if we are inside a plugin dir

--- packages/create-plugin/src/utils/utils.plugin.ts
+++ packages/create-plugin/src/utils/utils.plugin.ts
@@ -1,8 +1,25 @@
 import path from 'node:path';
 import { readJsonFile } from './utils.files.js';
+import { compileTemplateFiles, getTemplateData } from '../utils/utils.templates.js';
+import { UDPATE_CONFIG } from '../constants.js';
 
 export function getPluginJson(srcDir?: string) {
   const srcPath = srcDir || path.join(process.cwd(), 'src');
   const pluginJsonPath = path.join(srcPath, 'plugin.json');
   return readJsonFile(pluginJsonPath);
+}
+
+// Updates the .config/ directory with the latest templates
+export function updatePluginBuildConfig() {
+  compileTemplateFiles(UDPATE_CONFIG.filesToOverride, getTemplateData());
+}
+
+// Checks if CWD is a valid root directory of a plugin
+export function isPluginDirectory() {
+  try {
+    getPluginJson();
+    return true;
+  } catch (e) {
+    return false;
+  }
 }
