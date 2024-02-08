8db36471bdbca2f679552134eedee56b7302bd86
Erik Sundell <erik.sundell87@gmail.com>
2024-02-08 21:34:12 +0100

    revert config

--- packages/plugin-e2e/playwright.config.ts
+++ packages/plugin-e2e/playwright.config.ts
@@ -13,9 +13,9 @@ export default defineConfig<PluginOptions>({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 2 : undefined,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -25,31 +25,55 @@ export default defineConfig<PluginOptions>({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    httpCredentials: {
-      username: 'admin',
-      password: 'admin',
-    },
     featureToggles: {
       redshiftAsyncQueryDataSupport: false,
+    },
+    httpCredentials: {
+      username: '',
+      password: '',
     },
   },
 
   /* List of projects to run. See https://playwright.dev/docs/test-configuration#projects */
   projects: [
-    // 1. Login to Grafana and store the cookie on disk for use in other tests.
+    // Login to Grafana with admin user and store the cookie on disk for use in other tests
     {
       name: 'authenticate',
       testDir: './src/auth',
       testMatch: [/.*auth\.setup\.ts/],
     },
-    // 2. Run all tests in parallel using Chrome.
+    // Login to Grafana with new user with viewer role and store the cookie on disk for use in other tests
+    {
+      name: 'createUserAndAuthenticate',
+      testDir: './src/auth',
+      testMatch: [/.*auth\.setup\.ts/],
+      use: {
+        user: {
+          user: 'viewer',
+          password: 'password',
+          role: 'Viewer',
+        },
+      },
+    },
+    // Run all tests in parallel using user with admin role
     {
       name: 'admin',
+      testDir: './tests/admin',
       use: {
         ...devices['Desktop Chrome'],
-        storageState: 'playwright/.auth/user.json',
+        storageState: 'playwright/.auth/admin.json',
       },
       dependencies: ['authenticate'],
+    },
+    // Run all tests in parallel using user with viewer role
+    {
+      name: 'viewer',
+      testDir: './tests/viewer',
+      use: {
+        ...devices['Desktop Chrome'],
+        storageState: 'playwright/.auth/viewer.json',
+      },
+      dependencies: ['createUserAndAuthenticate'],
     },
   ],
 });
