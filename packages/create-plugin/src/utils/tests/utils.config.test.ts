0abb8bb671a92d41c016c679a6d52c13eac0cb51
Levente Balogh <balogh.levente.hu@gmail.com>
2024-02-16 14:27:52 +0100

    Wip

--- packages/create-plugin/src/utils/tests/utils.config.test.ts
+++ packages/create-plugin/src/utils/tests/utils.config.test.ts
@@ -1,7 +1,14 @@
 import fs from 'fs/promises';
 import path from 'path';
 import os from 'os';
+import { getVersion } from '../utils.version.js';
 import { getConfig, UserConfig, CreatePluginConfig } from '../utils.config.js';
+import { commandName } from '../utils.cli.js';
+
+// Mock "commandName" from "../utils.cli.js"
+jest.mock('../utils.cli.js', () => ({
+  commandName: jest.fn(),
+}));
 
 const tmpDir = path.join(os.tmpdir(), 'cp-test-config');
 
@@ -13,6 +20,11 @@ beforeAll(async () => {
 afterAll(async () => {
   // Clean up temporary directory
   await fs.rm(tmpDir, { recursive: true, force: true });
+});
+
+afterEach(() => {
+  jest.resetAllMocks();
+  (commandName as unknown as jest.Mock).mockReturnValue(() => 'generate');
 });
 
 describe('getConfig', () => {
@@ -40,5 +52,21 @@ describe('getConfig', () => {
 
     // Assert
     expect(config).toEqual({ ...rootConfig, ...userConfig });
+  });
+
+  describe('Command: Generate', () => {
+    it('should give back a default config when there are no .cprc.json files', async () => {
+      // Act
+      const config = getConfig(tmpDir);
+
+      // Assert
+      expect(config).toEqual({
+        version: getVersion(),
+        features: {
+          bundleGrafanaUI: false,
+          useReactRouterV6: false,
+        },
+      });
+    });
   });
 });
