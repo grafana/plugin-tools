ec12ab9a41898ddc05d4d833b0557b2a00ffea7b
Erik Sundell <erik.sundell87@gmail.com>
2024-02-19 09:23:00 +0100

    move dir to constant

--- packages/plugin-e2e/src/fixtures/commands/readProvisionedDashboard.ts
+++ packages/plugin-e2e/src/fixtures/commands/readProvisionedDashboard.ts
@@ -10,9 +10,11 @@ type ReadProvisionedDashboardFixture = TestFixture<
   PluginFixture & PluginOptions & PlaywrightCombinedArgs
 >;
 
+const DASHBOARDS_DIR = 'dashboards';
+
 const readProvisionedDashboard: ReadProvisionedDashboardFixture = async ({ provisioningRootDir }, use) => {
   await use(async ({ fileName }) => {
-    const resolvedPath = path.resolve(path.join(provisioningRootDir, 'dashboards', fileName));
+    const resolvedPath = path.resolve(path.join(provisioningRootDir, DASHBOARDS_DIR, fileName));
     const contents = await promises.readFile(resolvedPath, 'utf8');
     return JSON.parse(contents);
   });
