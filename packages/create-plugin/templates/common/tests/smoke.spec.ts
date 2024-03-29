473371fa8acddb8b07120fa97da5c041e1a08468
Jack Westbrook <jack.westbrook@gmail.com>
2024-03-29 17:02:24 +0100

    chore(create-plugin): await smoke test expect

--- packages/create-plugin/templates/common/tests/smoke.spec.ts
+++ packages/create-plugin/templates/common/tests/smoke.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@grafana/plugin-e2e';
 
 test('should load dashboard page', async ({ dashboardPage }) => {
-  expect(dashboardPage.ctx.page).toHaveTitle('New dashboard - Dashboards - Grafana');
+  await expect(dashboardPage.ctx.page).toHaveTitle('New dashboard - Dashboards - Grafana');
 });
