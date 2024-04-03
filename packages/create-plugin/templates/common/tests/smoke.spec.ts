a852b54059adc6e2191221c80af78c748ac11215
Erik Sundell <erik.sundell87@gmail.com>
2024-04-03 14:19:53 +0200

    add panel tests

--- packages/create-plugin/templates/common/tests/smoke.spec.ts
+++
@@ -1,5 +0,0 @@
-import { test, expect } from '@grafana/plugin-e2e';
-
-test('should load dashboard page', async ({ dashboardPage }) => {
-  await expect(dashboardPage.ctx.page).toHaveTitle('New dashboard - Dashboards - Grafana');
-});
