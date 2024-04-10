8ca92425d1f967c98cf35dd87056c349fe7c5897
Levente Balogh <balogh.levente.hu@gmail.com>
2024-04-10 16:05:55 +0200

    refactor: use emitAsset()

--- packages/plugin-meta-extractor/src/webpack-plugin.ts
+++ packages/plugin-meta-extractor/src/webpack-plugin.ts
@@ -5,7 +5,10 @@ import { extractPluginMeta } from './meta-extractor.js';
 const DEFAULT_GENERATED_FILENAME = 'plugin.generated.json';
 
 export type GrafanaPluginMetaExtractorOptions = {
-  entryFile?: string;
+  // Can be used to override the entry file path
+  entryFile: string;
+
+  // Can be used to override the output asset name
   outputAssetName?: string;
 };
 
@@ -20,14 +23,14 @@ export class GrafanaPluginMetaExtractor {
     compiler.hooks.emit.tapAsync('GrafanaPluginMetaExtractor', (compilation, callback) => {
       const { webpack } = compiler;
       const { RawSource } = webpack.sources;
-      const entryFile = this.options.entryFile || 'index.js';
+      const entryFile = this.options.entryFile;
       const entryFilePath = path.resolve(compiler.context, entryFile);
       const pluginMeta = extractPluginMeta(entryFilePath);
 
       const newAssetName = this.options.outputAssetName || DEFAULT_GENERATED_FILENAME;
       const newAssetContent = JSON.stringify(pluginMeta, null, 2);
 
-      compilation.assets[newAssetName] = new RawSource(newAssetContent);
+      compilation.emitAsset(newAssetName, new RawSource(newAssetContent));
 
       callback();
     });
