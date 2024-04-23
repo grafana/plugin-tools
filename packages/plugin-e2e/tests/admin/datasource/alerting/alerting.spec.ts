72f8965d2ef29170c3309a4f558f3cbfc34db293
Erik Sundell <erik.sundell87@gmail.com>
2024-04-23 08:10:17 +0200

    fix tests

--- packages/plugin-e2e/tests/admin/datasource/alerting/alerting.spec.ts
+++ packages/plugin-e2e/tests/admin/datasource/alerting/alerting.spec.ts
@@ -4,7 +4,7 @@ test('should evaluate to true if query is valid', async ({ page, alertRuleEditPa
   const queryA = alertRuleEditPage.getAlertRuleQueryRow('A');
   await queryA.datasource.set('AWS Redshift');
   await page.waitForFunction(() => window.monaco);
-  await queryA.getByTestIdOrAriaLabel(selectors.components.CodeEditor.container).click();
+  await queryA.getByGrafanaSelector(selectors.components.CodeEditor.container).click();
   await page.keyboard.insertText('select * from long_format_example');
   await expect(alertRuleEditPage.evaluate()).toBeOK();
 });
@@ -13,18 +13,12 @@ test('should evaluate to false if query is invalid', async ({ page, alertRuleEdi
   const queryA = alertRuleEditPage.getAlertRuleQueryRow('A');
   await queryA.datasource.set('AWS Redshift');
   await page.waitForFunction(() => window.monaco);
-  await queryA.getByTestIdOrAriaLabel(selectors.components.CodeEditor.container).click();
+  await queryA.getByGrafanaSelector(selectors.components.CodeEditor.container).click();
   await page.keyboard.insertText('select !');
   await expect(alertRuleEditPage.evaluate()).not.toBeOK();
 });
 
-test('should evaluate to true when loading a provisioned query that is invalid', async ({
-  page,
-  selectors,
-  grafanaVersion,
-  request,
-}) => {
-  const alertRuleEditPage = new AlertRuleEditPage({ request, page, selectors, grafanaVersion }, { uid: 'SKfRaUhSz' });
-  await alertRuleEditPage.goto();
+test('should evaluate to true when loading a provisioned query that is valid', async ({ gotoAlertRuleEditPage }) => {
+  const alertRuleEditPage = await gotoAlertRuleEditPage({ uid: 'SKfRaUhSz' });
   await expect(alertRuleEditPage.evaluate()).toBeOK();
 });
