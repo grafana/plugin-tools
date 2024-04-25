316e5d8a4718ced6684d3ee9f6422fbed903a051
Marcus Andersson <marcus.andersson@grafana.com>
2024-04-25 09:27:21 +0200

    Create plugin: update scaffolded tests when creating an app plugin (#892)

--- packages/create-plugin/templates/app/tests/appNavigation.spec.ts
+++ packages/create-plugin/templates/app/tests/appNavigation.spec.ts
@@ -1,26 +1,25 @@
-import pluginJson from '../src/plugin.json';
-import { test, expect } from '@grafana/plugin-e2e';
+import { test, expect } from './fixtures';
 import { ROUTES } from '../src/constants';
 
 test.describe('navigating app', () => {
-  test('page one should render successfully', async ({ page }) => {
-    await page.goto(`/a/${pluginJson.id}/${ROUTES.One}`);
+  test('page one should render successfully', async ({ gotoPage, page }) => {
+    await gotoPage(`/${ROUTES.One}`);
     await expect(page.getByText('This is page one.')).toBeVisible();
   });
 
-  test('page two should render successfully', async ({ page }) => {
-    await page.goto(`/a/${pluginJson.id}/${ROUTES.Two}`);
+  test('page two should render successfully', async ({ gotoPage, page }) => {
+    await gotoPage(`/${ROUTES.Two}`);
     await expect(page.getByText('This is page two.')).toBeVisible();
   });
 
-  test('page three should support an id parameter', async ({ page }) => {
-    await page.goto(`/a/${pluginJson.id}/${ROUTES.Three}/123456`);
+  test('page three should support an id parameter', async ({ gotoPage, page }) => {
+    await gotoPage(`/${ROUTES.Three}/123456`);
     await expect(page.getByText('ID: 123456')).toBeVisible();
   });
 
-  test('page three should render sucessfully', async ({ page }) => {
+  test('page three should render sucessfully', async ({ gotoPage, page }) => {
     // wait for page to successfully render
-    await page.goto(`/a/${pluginJson.id}/${ROUTES.One}`);
+    await gotoPage(`/${ROUTES.One}`);
     await expect(page.getByText('This is page one.')).toBeVisible();
 
     // navigating to page four with full width layout without sidebar menu
