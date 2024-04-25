2b659f7234fcd2576b8da5c29a48bd110d5a500b
Erik Sundell <erik.sundell87@gmail.com>
2024-04-25 09:51:25 +0200

    add tsdoc

--- packages/plugin-e2e/src/models/pages/AlertRuleEditPage.ts
+++ packages/plugin-e2e/src/models/pages/AlertRuleEditPage.ts
@@ -18,6 +18,9 @@ export class AlertRuleEditPage extends GrafanaPage {
     return super.navigate(url, options);
   }
 
+  /**
+   * Returns a locator for hte alert rule name field
+   */
   get alertRuleNameField() {
     if (semver.gte(this.ctx.grafanaVersion, '11.1.0')) {
       return this.getByGrafanaSelector(this.ctx.selectors.components.AlertRules.ruleNameField);
@@ -26,6 +29,9 @@ export class AlertRuleEditPage extends GrafanaPage {
     return this.ctx.page.getByPlaceholder('Give your alert rule a name');
   }
 
+  /**
+   * Returns an instance of the {@link AlertRuleQuery} class for the query row with the provided refId.
+   */
   getAlertRuleQueryRow(refId: string): AlertRuleQuery {
     const locator = this.getByGrafanaSelector(this.ctx.selectors.components.QueryEditorRows.rows).filter({
       has: this.getByGrafanaSelector(this.ctx.selectors.components.QueryEditorRow.title(refId)),
@@ -34,7 +40,14 @@ export class AlertRuleEditPage extends GrafanaPage {
     return new AlertRuleQuery(this.ctx, locator);
   }
 
+  /**
+   * Clicks the evaluate button and waits for the evaluation to complete. If the evaluation is successful, the status code of the response is 200.
+   * If one or more queries are invalid, an error status code is returned.
+   */
   async evaluate(options?: RequestOptions) {
+    // Starting from Grafana 10.0.0, the alerting evaluation endpoint started returning errors in a different way.
+    // Even if one or many of the queries is failed, the status code for the http response is 200 so we have to check the status of each query instead.
+    // If at least one query is failed, we the response of the evaluate method is mapped to the status of the first failed query.
     if (semver.gte(this.ctx.grafanaVersion, '10.0.0')) {
       this.ctx.page.route(this.ctx.selectors.apis.Alerting.eval, async (route) => {
         const response = await route.fetch();
