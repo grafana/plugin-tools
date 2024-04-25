f72b2c45f1e1640902f77f07a21de1f5c3683fa9
Erik Sundell <erik.sundell87@gmail.com>
2024-04-25 09:08:30 +0200

    fix broken tests

--- packages/plugin-e2e/src/models/pages/AlertRuleEditPage.ts
+++ packages/plugin-e2e/src/models/pages/AlertRuleEditPage.ts
@@ -2,6 +2,7 @@ import * as semver from 'semver';
 import { AlertRuleArgs, NavigateOptions, PluginTestCtx, RequestOptions } from '../../types';
 import { GrafanaPage } from './GrafanaPage';
 import { AlertRuleQuery } from '../components/AlertRuleQuery';
+import { v4 as uuidv4 } from 'uuid';
 
 export class AlertRuleEditPage extends GrafanaPage {
   constructor(readonly ctx: PluginTestCtx, readonly args?: AlertRuleArgs) {
@@ -16,6 +17,55 @@ export class AlertRuleEditPage extends GrafanaPage {
     const url = this.args?.uid ? EditAlertRule.url(this.args.uid) : AddAlertRule.url;
 
     return super.navigate(url, options);
+  }
+
+  get alertRuleNameField() {
+    if (semver.gte(this.ctx.grafanaVersion, '11.1.0')) {
+      return this.getByGrafanaSelector(this.ctx.selectors.components.AlertRules.ruleNameField);
+    }
+
+    return this.ctx.page.getByPlaceholder('Give your alert rule a name');
+  }
+
+  async addNewRuleFolder(folderName?: string) {
+    const { AlertRules } = this.ctx.selectors.components;
+
+    let newFolderButton = semver.lt(this.ctx.grafanaVersion, '11.1.0')
+      ? this.ctx.page.getByRole('button', { name: 'New folder' })
+      : this.getByGrafanaSelector(AlertRules.newFolderButton);
+
+    await newFolderButton.click();
+    (await newFolderButton.isVisible()) && (await newFolderButton.click());
+
+    let dialog = this.ctx.page.getByRole('dialog');
+    let root = (await dialog.count()) > 1 ? dialog.last() : dialog;
+    let newFolderNameField = this.getByGrafanaSelector(AlertRules.newFolderNameField, { root });
+    let newFolderNameCreateButton = this.getByGrafanaSelector(AlertRules.newFolderNameCreateButton, { root });
+    if (semver.lt(this.ctx.grafanaVersion, '11.1.0')) {
+      newFolderButton = this.ctx.page.getByRole('button', { name: 'New folder' });
+      newFolderNameField = this.ctx.page.getByPlaceholder('Enter a name');
+      newFolderNameCreateButton = this.ctx.page.getByRole('button', { name: 'Create' });
+    }
+    await newFolderNameField.fill(folderName ?? uuidv4());
+    await newFolderNameCreateButton.click();
+  }
+
+  async addNewEvaluationGroup(options?: { interval?: string; groupName?: string }) {
+    const { AlertRules } = this.ctx.selectors.components;
+    let newEvaluationGroupButton = this.getByGrafanaSelector(AlertRules.newEvaluationGroupButton);
+    let newEvaluationGroupName = this.getByGrafanaSelector(AlertRules.newEvaluationGroupName);
+    let newEvaluationGroupInterval = this.getByGrafanaSelector(AlertRules.newEvaluationGroupInterval);
+    let newEvaluationGroupCreate = this.getByGrafanaSelector(AlertRules.newEvaluationGroupCreate);
+    if (semver.lt(this.ctx.grafanaVersion, '11.1.0')) {
+      newEvaluationGroupButton = this.ctx.page.getByRole('button', { name: 'New evaluation group' });
+      newEvaluationGroupName = this.ctx.page.getByPlaceholder('Enter a name');
+      newEvaluationGroupInterval = this.ctx.page.getByPlaceholder('e.g. 5m');
+      newEvaluationGroupCreate = this.ctx.page.getByRole('button', { name: 'Create' });
+    }
+    await newEvaluationGroupButton.click();
+    await newEvaluationGroupName.fill(options?.groupName ?? uuidv4());
+    await newEvaluationGroupInterval.fill(options?.interval ?? '5m');
+    await newEvaluationGroupCreate.click();
   }
 
   getAlertRuleQueryRow(refId: string): AlertRuleQuery {
@@ -44,7 +94,22 @@ export class AlertRuleEditPage extends GrafanaPage {
       options
     );
 
-    await this.ctx.page.getByRole('button', { name: 'Preview', exact: true }).click();
+    let evaluateButton = this.getByGrafanaSelector(this.ctx.selectors.components.AlertRules.previewButton);
+    if (semver.lt(this.ctx.grafanaVersion, '11.1.0')) {
+      evaluateButton = this.ctx.page.getByRole('button', { name: 'Preview', exact: true });
+    }
+    const evalReq = this.ctx.page
+      .waitForRequest((req) => req.url().includes(this.ctx.selectors.apis.Alerting.eval), {
+        timeout: 1000,
+      })
+      .catch(async () => {
+        // it seems (intermittently) the first click doesn't trigger the request, so we click again
+        await evaluateButton.click();
+      });
+
+    await evaluateButton.click();
+    await evalReq;
+
     return responsePromise;
   }
 }
---
+++ packages/plugin-e2e/tests/as-admin-user/datasource/alerting/alerting.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from '../../../../src';
+
+test('should evaluate to true if query is valid', async ({ page, alertRuleEditPage, selectors }) => {
+  const queryA = alertRuleEditPage.getAlertRuleQueryRow('A');
+  await queryA.datasource.set('AWS Redshift');
+  await alertRuleEditPage.alertRuleNameField.fill('Test Alert Rule');
+  await page.waitForFunction(() => window.monaco);
+  await queryA.getByGrafanaSelector(selectors.components.CodeEditor.container).click();
+  await page.keyboard.insertText('select * from long_format_example');
+  // await alertRuleEditPage.addNewRuleFolder();
+  // await alertRuleEditPage.addNewEvaluationGroup();
+  await expect(alertRuleEditPage.evaluate()).toBeOK();
+});
+
+test('should evaluate to false if query is invalid', async ({ page, alertRuleEditPage, selectors }) => {
+  const queryA = alertRuleEditPage.getAlertRuleQueryRow('A');
+  await queryA.datasource.set('AWS Redshift');
+  await page.waitForFunction(() => window.monaco);
+  await alertRuleEditPage.alertRuleNameField.fill('Test Alert Rule');
+  await queryA.getByGrafanaSelector(selectors.components.CodeEditor.container).click();
+  await page.keyboard.insertText('select !');
+  // await alertRuleEditPage.addNewRuleFolder();
+  // await alertRuleEditPage.addNewEvaluationGroup();
+  await expect(alertRuleEditPage.evaluate()).not.toBeOK();
+});
+
+test('should evaluate to true when loading a provisioned query that is valid', async ({
+  gotoAlertRuleEditPage,
+  readProvisionedAlertRule,
+}) => {
+  const alertRule = await readProvisionedAlertRule({ fileName: 'alerts.yml' });
+  const alertRuleEditPage = await gotoAlertRuleEditPage(alertRule);
+  await expect(alertRuleEditPage.evaluate()).toBeOK();
+});
+
+test('should evaluate to false when loading a provisioned query that is invalid', async ({
+  gotoAlertRuleEditPage,
+  readProvisionedAlertRule,
+}) => {
+  const alertRule = await readProvisionedAlertRule({ fileName: 'alerts.yml', ruleTitle: 'broken_rule' });
+  const alertRuleEditPage = await gotoAlertRuleEditPage(alertRule);
+  await expect(alertRuleEditPage.evaluate()).not.toBeOK();
+});
