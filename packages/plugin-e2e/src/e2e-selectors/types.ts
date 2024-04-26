9c475df5f4819cdd52bdd4e50db92f89e5ce4e6b
Erik Sundell <erik.sundell87@gmail.com>
2024-04-26 09:56:20 +0200

    add correct type

--- packages/plugin-e2e/src/e2e-selectors/types.ts
+++ packages/plugin-e2e/src/e2e-selectors/types.ts
@@ -93,6 +93,7 @@ export type Components = {
   };
   QueryEditorRow: {
     title: (refId: string) => string;
+    actionButton: (title: string) => string;
   };
   AlertRules: {
     previewButton: string;
