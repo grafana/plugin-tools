13f3c5cd00640b2f1903ef438391c30f82c1d535
Andreas Christou <andreas.christou@grafana.com>
2024-06-03 18:32:02 +0100

    Fix deleteDashboard API

--- packages/plugin-e2e/src/e2e-selectors/versioned/apis.ts
+++ packages/plugin-e2e/src/e2e-selectors/versioned/apis.ts
@@ -29,7 +29,7 @@ export const versionedAPIs = {
   },
   Dashboard: {
     delete: {
-      [MIN_GRAFANA_VERSION]: (uid: string) => `/api/datasources/uid/${uid}`,
+      [MIN_GRAFANA_VERSION]: (uid: string) => `/api/dashboards/uid/${uid}`,
     },
   },
   Plugin: {
