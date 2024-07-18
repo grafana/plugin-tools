a11f5b723317f988fedf7c6381ff1732a71ed4e2
Jack Westbrook <jack.westbrook@gmail.com>
2024-07-18 12:55:33 +0200

    feat(create-plugin): use message so correct values live in answers, remove description

--- packages/create-plugin/src/commands/generate/prompt-user.ts
+++ packages/create-plugin/src/commands/generate/prompt-user.ts
@@ -7,16 +7,16 @@ export async function promptUser(argv: minimist.ParsedArgs) {
   let answers: Partial<GenerateCliArgs> = {};
   const enquirer = new Enquirer();
 
-  for (const prompt of prompts) {
-    const { name, shouldPrompt } = prompt(answers);
+  for (const p of prompts) {
+    const prompt = p(answers);
 
-    if (argv.hasOwnProperty(name)) {
-      answers = { ...answers, [name]: argv[name] };
+    if (argv.hasOwnProperty(prompt.name)) {
+      answers = { ...answers, [prompt.name]: argv[prompt.name] };
     } else {
-      if (typeof shouldPrompt === 'function' && !shouldPrompt(answers)) {
+      if (typeof prompt.shouldPrompt === 'function' && !prompt.shouldPrompt(answers)) {
         continue;
       } else {
-        const result = await enquirer.prompt(prompt(answers));
+        const result = await enquirer.prompt(prompt);
         answers = { ...answers, ...result };
       }
     }
@@ -36,7 +36,7 @@ type Prompt = {
 };
 
 type Choice = {
-  name: string;
+  name?: string;
   message?: string;
   value?: unknown;
   hint?: string;
@@ -51,34 +51,38 @@ const prompts: Array<(answers: Partial<GenerateCliArgs>) => Prompt> = [
     type: 'select',
     choices: [
       {
-        name: 'App (Custom pages, UI Extensions and bundling other plugins)',
+        message: 'App (Custom pages, UI Extensions and bundling other plugins)',
         value: PLUGIN_TYPES.app,
       },
       {
-        name: 'Data source (Query data from a custom source)',
+        message: 'Data source (Query data from a custom source)',
         value: PLUGIN_TYPES.datasource,
       },
       {
-        name: 'Panel (New visualization for data or a widget)',
+        message: 'Panel (New visualization for data or a widget)',
         value: PLUGIN_TYPES.panel,
       },
       {
-        name: 'App with Scenes (Create dynamic dashboards in app pages)',
+        message: 'App with Scenes (Create dynamic dashboards in app pages)',
         value: PLUGIN_TYPES.scenes,
       },
     ],
-    message: 'Select plugin type',
+    message: 'Select a plugin type',
   }),
-  (answers) => ({
-    name: 'hasBackend',
-    type: 'confirm',
-    message:
-      answers.pluginType === PLUGIN_TYPES.app
-        ? 'Does your plugin require a backend to support server-side functionality (e.g. calling external APIs, custom backend logic, advanced authentication, etc)?'
-        : 'Does your plugin require a backend to support server-side functionality (e.g. alerting, advanced authentication, public dashboards, etc)?',
-    initial: false,
-    shouldPrompt: (answers) => answers.pluginType !== PLUGIN_TYPES.panel,
-  }),
+  (answers) => {
+    const isAppType = answers.pluginType === PLUGIN_TYPES.app || answers.pluginType === PLUGIN_TYPES.scenes;
+    const message = isAppType
+      ? 'Does your plugin require a backend to support server-side functionality? (e.g. calling external APIs, custom backend logic, advanced authentication, etc.)'
+      : 'Does your plugin require a backend to support server-side functionality? (e.g. alerting, advanced authentication, public dashboards, etc.)';
+
+    return {
+      name: 'hasBackend',
+      type: 'confirm',
+      message: message,
+      initial: false,
+      shouldPrompt: (answers) => answers.pluginType !== PLUGIN_TYPES.panel,
+    };
+  },
   () => ({
     name: 'pluginName',
     type: 'input',
@@ -100,11 +104,5 @@ const prompts: Array<(answers: Partial<GenerateCliArgs>) => Prompt> = [
       }
       return 'Organization name is required';
     },
-  }),
-  () => ({
-    name: 'pluginDescription',
-    type: 'input',
-    message: 'How would you describe your plugin?',
-    initial: '',
   }),
 ];
