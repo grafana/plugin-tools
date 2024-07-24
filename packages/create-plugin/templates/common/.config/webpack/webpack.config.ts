0dd26520ff30965c647bb59fce5a1cb76251ccd3
Jack Westbrook <jack.westbrook@gmail.com>
2024-07-24 15:33:34 +0200

    feat(create-plugin): make crossOrigin loading anonymous for SRI plugin to work

--- packages/create-plugin/templates/common/.config/webpack/webpack.config.ts
+++ packages/create-plugin/templates/common/.config/webpack/webpack.config.ts
@@ -183,6 +183,7 @@ const config = async (env): Promise<Configuration> => {
       path: path.resolve(process.cwd(), DIST_DIR),
       publicPath: `public/plugins/${pluginJson.id}/`,
       uniqueName: pluginJson.id,
+      crossOriginLoading: 'anonymous',
     },
 
     plugins: [
