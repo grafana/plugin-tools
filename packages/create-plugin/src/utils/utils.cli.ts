86f71fc5b2fd194e50fd299f0d5b0251e77ba79d
Jack Westbrook <jack.westbrook@gmail.com>
2024-08-06 23:29:51 +0200

    fix(create-plugin): remove unknown minimist setting so command names work

--- packages/create-plugin/src/utils/utils.cli.ts
+++ packages/create-plugin/src/utils/utils.cli.ts
@@ -1,5 +1,4 @@
 import minimist from 'minimist';
-import { printWarning } from './utils.console.js';
 
 export const args = process.argv.slice(2);
 
@@ -11,10 +10,6 @@ export const argv = minimist(args, {
     hasBackend: 'backend',
     pluginName: 'plugin-name',
     orgName: 'org-name',
-  },
-  unknown: (arg) => {
-    printWarning(`Ignoring unknown option: ${arg}.`);
-    return false;
   },
 });
 
