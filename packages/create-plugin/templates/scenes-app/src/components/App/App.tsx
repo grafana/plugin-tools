8dc73b46d97cb25500de8436052719b181cc4d24
Jack Westbrook <jack.westbrook@gmail.com>
2024-08-23 15:29:35 +0200

    fix(create-plugin): fix app component in scenes templates

--- packages/create-plugin/templates/scenes-app/src/components/App/App.tsx
+++ packages/create-plugin/templates/scenes-app/src/components/App/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { AppRootProps } from '@grafana/data';
 import { PluginPropsContext } from '../../utils/utils.plugin';
-import { Routes } from '../Routes';
+import { Routes } from '../Routes/Routes';
 
 export function App(props: AppRootProps) {
   return (
-    <PluginPropsContext.Provider value={this.props}>
+    <PluginPropsContext.Provider value={props}>
       <Routes />
     </PluginPropsContext.Provider>
   );
