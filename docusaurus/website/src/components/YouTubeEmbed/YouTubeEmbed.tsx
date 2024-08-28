c325cb7a773a4f09d366e32081f05969e9e4ec36
Tom Glenn <tom.glenn@grafana.com>
2024-08-28 10:09:08 +0100

    Type hint that props are required

--- docusaurus/website/src/components/YouTubeEmbed/YouTubeEmbed.tsx
+++ docusaurus/website/src/components/YouTubeEmbed/YouTubeEmbed.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 // Define the type to control the props that can be passed to the YouTubeEmbed component.
 type YouTubeEmbedProps = {
   // The YouTube video Id to embed.
-  videoId?: string;
+  videoId: string;
   // The title of the YouTube video for accessibility.
-  title?: string;
+  title: string;
 };
 
 // YouTubeEmbed component that accepts the specified props.
-export default function YouTubeEmbed({ videoId = '', title = 'YouTube video player' }: YouTubeEmbedProps) {
+export default function YouTubeEmbed({ videoId, title = 'YouTube video player' }: YouTubeEmbedProps) {
   if (videoId) {
     return (
       <iframe
