402b306b6d0ab26b33b0edb64090f6b124bc2c98
Jack Westbrook <jack.westbrook@gmail.com>
2024-08-30 16:28:58 +0200

    feat(crete-plugin): make use of lazy and suspense to code split module.tsx for apps

--- packages/create-plugin/templates/app/src/module.tsx
+++ packages/create-plugin/templates/app/src/module.tsx
@@ -1,10 +1,26 @@
-import { lazy } from 'react';
-import { AppPlugin } from '@grafana/data';
-import { App } from './components/App/App';
+import React, { Suspense, lazy } from 'react';
+import { AppPlugin, type AppRootProps } from '@grafana/data';
+import { LoadingPlaceholder } from '@grafana/ui';
+import type { AppConfigProps } from './components/AppConfig/AppConfig';
+
+const LazyApp = lazy(() => import('./components/App/App'));
+const LazyAppConfig = lazy(() => import('./components/AppConfig/AppConfig'));
+
+const App = (props: AppRootProps) => (
+  <Suspense fallback={<LoadingPlaceholder text="" />}>
+    <LazyApp {...props} />
+  </Suspense>
+);
+
+const AppConfig = (props: AppConfigProps) => (
+  <Suspense fallback={<LoadingPlaceholder text="" />}>
+    <LazyAppConfig {...props} />
+  </Suspense>
+);
 
 export const plugin = new AppPlugin<{}>().setRootPage(App).addConfigPage({
   title: 'Configuration',
   icon: 'cog',
-  body: lazy(() => import('./components/AppConfig/AppConfig')),
+  body: AppConfig,
   id: 'configuration',
 });
--- packages/create-plugin/templates/scenes-app/src/components/App/App.tsx
+++ packages/create-plugin/templates/scenes-app/src/components/App/App.tsx
@@ -3,10 +3,12 @@ import { AppRootProps } from '@grafana/data';
 import { PluginPropsContext } from '../../utils/utils.plugin';
 import { Routes } from '../Routes/Routes';
 
-export function App(props: AppRootProps) {
+function App(props: AppRootProps) {
   return (
     <PluginPropsContext.Provider value={props}>
       <Routes />
     </PluginPropsContext.Provider>
   );
 }
+
+export default App;
