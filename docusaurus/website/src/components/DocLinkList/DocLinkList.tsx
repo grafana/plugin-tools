5b3c8070b4538a7da6bef4fc326900108819b82c
Jack Westbrook <jack.westbrook@gmail.com>
2024-09-04 16:38:49 +0200

    refactor(website): update import locations for from theme-common/internal to plugin-content-docs

--- docusaurus/website/src/components/DocLinkList/DocLinkList.tsx
+++ docusaurus/website/src/components/DocLinkList/DocLinkList.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { useCurrentSidebarCategory, filterDocCardListItems } from '@docusaurus/theme-common';
-import { useDocById, findFirstSidebarItemLink } from '@docusaurus/theme-common/internal';
+import {
+  useCurrentSidebarCategory,
+  filterDocCardListItems,
+  useDocById,
+  findFirstSidebarItemLink,
+} from '@docusaurus/plugin-content-docs/client';
 import Link from '@docusaurus/Link';
 import type { Props } from '@theme/DocCardList';
 import styles from './styles.module.css';
