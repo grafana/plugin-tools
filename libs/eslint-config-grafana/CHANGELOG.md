14215eb1605d0d2f8123aa8ee973c53c8ce73571
Jack Westbrook <jack.westbrook@gmail.com>
2024-10-17 16:27:47 +0200

    Merge pull request #41 from grafana/jackw/support-eslint-9
    Feat: Add flat config for Eslint 9 support

---
+++ libs/eslint-config-grafana/CHANGELOG.md
@@ -0,0 +1,105 @@
+# Grafana Eslint config changelog
+
+## 8.0.0
+
+- **Breaking**: Migrate dependencies to peerDependencies. Please make sure to install peerDependencies when using this package.
+- Add flat config for Eslint 9 support.
+
+## 7.0.0
+
+- Bump dependencies.
+
+## 6.0.1
+
+- Remove @typescript-eslint/explicit-member-accessibility rule.
+
+## 6.0.0
+
+- Bump dependencies.
+
+## 5.1.0
+
+- Bump dependencies to support Node 18.
+
+## 5.0.0
+
+- Add no-var rule.
+
+## 4.0.0
+
+- Bump dependencies.
+
+## 3.0.0
+
+- Remove eslint-plugin-prettier.
+
+## 2.5.2
+
+- Bump dependencies.
+
+## 2.5.1
+
+- Update eslint-config-prettier and eslint-plugin-jsdoc.
+- Report react-hooks/exhaustive-deps as error.
+
+## 2.5.0
+
+- Update typescript to 4.3.4.
+- Update @typescript-eslint/eslint-plugin and @typescript-eslint/parser.
+
+## 2.4.0
+
+- Update typescript to 4.2.4.
+- Update @typescript-eslint/eslint-plugin and @typescript-eslint/parser.
+
+## 2.3.0
+
+- Update Eslint dependency.
+
+## 2.2.1
+
+- Bump dependencies.
+
+## 2.2.0
+
+- Add no-duplicate-imports.
+
+## 2.1.1
+
+- Update eslint-plugin-prettier.
+
+## 2.1.0
+
+- Update Prettier and eslint-config-prettier.
+
+## 2.0.6
+
+- Adds eslint-plugin-react with recommended setings enabled.
+
+## 2.0.5
+
+- Allow console info statements.
+
+## 2.0.4
+
+- Bump eslint-plugin-react-hooks to 4.1.2.
+
+## 2.0.3
+
+- Pin dependencies to specific versions.
+
+## 2.0.2
+
+- Migrate peerDependencies to dependencies.
+
+## 2.0.1
+
+- Bump dependencies.
+
+## 2.0.0
+
+- Bump dependencies.
+
+## 1.0.0-rc1
+
+Initial release candidate.
--- libs/eslint-config-grafana/index.js
+++ libs/eslint-config-grafana/index.js
@@ -1,86 +1,18 @@
-"use strict";
+const baseConfig = require("./base");
 
 module.exports = {
   parserOptions: {
-    ecmaVersion: 2019,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
+    ecmaVersion: baseConfig.ecmaVersion,
+    sourceType: baseConfig.sourceType,
+    ...baseConfig.parserOptions,
   },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-  plugins: ["jsdoc", "@typescript-eslint", "react-hooks"],
+  settings: baseConfig.settings,
+  plugins: ["jsdoc", "@typescript-eslint", "@stylistic/ts", "react-hooks"],
   extends: [
     "plugin:react-hooks/recommended",
     "plugin:react/recommended",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
-  rules: {
-    curly: "error",
-    "dot-notation": "off",
-    "eol-last": "error",
-    eqeqeq: ["error", "always", { null: "ignore" }],
-    "guard-for-in": "off",
-    "jsdoc/check-alignment": "error",
-    "new-parens": "error",
-    "no-array-constructor": "error",
-    "no-bitwise": "off",
-    "no-caller": "error",
-    "no-cond-assign": "error",
-    "no-console": ["error", { allow: ["error", "log", "warn", "info"] }],
-    "no-debugger": "error",
-    "no-empty": "off",
-    "no-eval": "error",
-    "no-fallthrough": "off",
-    "no-new-wrappers": "error",
-    "no-redeclare": "error",
-    "no-restricted-imports": ["error", "moment"],
-    "no-shadow": "off",
-    "no-unused-expressions": "off",
-    "no-unused-labels": "error",
-    "no-var": "error",
-    radix: "error",
-    "sort-keys": "off",
-    "spaced-comment": ["off", "always"],
-    "use-isnan": "error",
-    "no-duplicate-imports": "error",
-    "@typescript-eslint/no-unused-expressions": [
-      "error",
-      { allowShortCircuit: true, allowTernary: true },
-    ],
-    "@typescript-eslint/array-type": ["error", { default: "array-simple" }],
-    "@typescript-eslint/naming-convention": [
-      "error",
-      {
-        selector: "interface",
-        format: ["PascalCase"],
-        custom: {
-          regex: "^I[A-Z]",
-          match: false,
-        },
-      },
-    ],
-    "@typescript-eslint/consistent-type-assertions": "error",
-    "@typescript-eslint/no-inferrable-types": "error",
-    "@typescript-eslint/no-namespace": ["error", { allowDeclarations: false }],
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/no-use-before-define": "off",
-    "@typescript-eslint/triple-slash-reference": "error",
-    "@typescript-eslint/type-annotation-spacing": [
-      "error",
-      {
-        after: true,
-        before: false,
-        overrides: {
-          arrow: { after: true, before: true },
-        },
-      },
-    ],
-    "react-hooks/exhaustive-deps": "error",
-  },
+  rules: baseConfig.rules,
 };
