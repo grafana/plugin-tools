fc8397c8e4286fd10beee04bf62a408f74f4f3ee
Jack Westbrook <jack.westbrook@gmail.com>
2024-11-28 12:59:31 +0100

    Wip

---
+++ .github/knip.js
@@ -0,0 +1,14 @@
+const { resolve } = require('node:path');
+
+const cwd = process.cwd();
+
+module.exports = {
+  entry: [resolve(cwd, 'src/module.{js,ts,tsx}')],
+  project: [resolve(cwd, 'src/**/*'), resolve(cwd, 'tests/**/*'), resolve(cwd, '.config/**/*')],
+  eslint: {
+    config: [resolve(cwd, '.config/.eslintrc'), resolve(cwd, './.eslintrc')],
+  },
+  webpack: {
+    config: [resolve(cwd, '.config/webpack/webpack.config.ts')],
+  },
+};
