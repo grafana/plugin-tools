5e0956aa8dcff5f80e67b152403e032842916e41
Jack Westbrook <jack.westbrook@gmail.com>
2024-11-28 14:46:03 +0100

    Wip

--- .github/knip.js
+++ .github/knip.js
@@ -2,6 +2,8 @@ const { resolve } = require('node:path');
 
 const cwd = process.cwd();
 
+console.log(resolve(cwd, '.config/webpack/webpack.config.ts'));
+
 module.exports = {
   entry: [resolve(cwd, 'src/module.{js,ts,tsx}')],
   project: [resolve(cwd, 'src/**/*'), resolve(cwd, 'tests/**/*'), resolve(cwd, '.config/**/*')],
