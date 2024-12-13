5a9af9f1d2abd5070ffc11327cc2135c8b8e2744
Erik Sundell <erik.sundell87@gmail.com>
2024-12-13 13:05:22 +0100

    test variables

---
+++ packages/plugin-e2e/tests/as-admin-user/datasource/variables/variableEditor.spec.ts
@@ -0,0 +1,34 @@
+import { expect, test } from '../../../../src';
+
+test('should render variable editor', async ({ variableEditPage, page, readProvisionedDataSource }) => {
+  const ds = await readProvisionedDataSource({ fileName: 'testdatasource.yaml' });
+  await variableEditPage.datasource.set(ds.name);
+  await expect(page.getByRole('textbox', { name: 'Query Text' })).toBeVisible();
+});
+
+test('create new variable and execute successful query', async ({
+  variableEditPage,
+  readProvisionedDataSource,
+  page,
+}) => {
+  const ds = await readProvisionedDataSource({ fileName: 'testdatasource.yaml' });
+  await variableEditPage.datasource.set(ds.name);
+  await page.getByRole('textbox', { name: 'Query Text' }).fill('annotationQuery');
+  const queryDataRequest = variableEditPage.waitForQueryDataRequest();
+  await variableEditPage.runQuery();
+  await queryDataRequest;
+  await expect(variableEditPage).toDisplayPreviews(['A', 'B']);
+});
+
+test('open existing variable and execute successful query', async ({
+  gotoVariableEditPage,
+  readProvisionedDashboard,
+  page,
+}) => {
+  const dashboard = await readProvisionedDashboard({ fileName: 'test-datasource.json' });
+  const variableEditPage = await gotoVariableEditPage({ dashboard, id: '0' });
+  const queryDataRequest = variableEditPage.waitForQueryDataRequest();
+  await variableEditPage.runQuery();
+  await queryDataRequest;
+  await expect(variableEditPage).toDisplayPreviews(['A', 'B']);
+});
