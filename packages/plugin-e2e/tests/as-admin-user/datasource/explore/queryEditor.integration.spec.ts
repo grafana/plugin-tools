964e6812996192b4663407f8d6f95820261102c7
Erik Sundell <erik.sundell87@gmail.com>
2024-12-18 14:31:42 +0100

    fix more tests

--- packages/plugin-e2e/tests/as-admin-user/datasource/explore/queryEditor.integration.spec.ts
+++ packages/plugin-e2e/tests/as-admin-user/datasource/explore/queryEditor.integration.spec.ts
@@ -26,13 +26,11 @@ test('explore page should display table and time series panel only for certain q
   explorePage,
   grafanaVersion,
 }) => {
-  const url = semver.lt('10.0.0', grafanaVersion)
-    ? `schemaVersion=1&panes=%7B%22oae%22:%7B%22datasource%22:%22--%20Mixed%20--%22,%22queries%22:%5B%7B%22constant%22:9,%22refId%22:%22A%22,%22datasource%22:%7B%22type%22:%22grafana-test-datasource%22,%22uid%22:%22P6E498B96656A7F9B%22%7D,%22queryText%22:%22test%20query%22,%22project%22:%22project-2%22%7D%5D,%22range%22:%7B%22from%22:%22now-1h%22,%22to%22:%22now%22%7D%7D%7D&orgId=1`
-    : 'schemaVersion=1&panes=%7B"oae":%7B"datasource":"--%20Mixed%20--","queries":%5B%7B"constant":9,"refId":"A","datasource":%7B"type":"grafana-test-datasource","uid":"P6E498B96656A7F9B"%7D,"queryText":"test%20query","project":"project-2"%7D%5D,"range":%7B"from":"now-1h","to":"now"%7D%7D%7D&orgId=1&left=%7B"datasource":"P6E498B96656A7F9B","queries":%5B%7B"refId":"A","datasource":%7B"type":"grafana-test-datasource","uid":"P6E498B96656A7F9B"%7D,"constant":6.5,"project":"project-1","queryText":"some%20query"%7D%5D,"range":%7B"from":"now-1h","to":"now"%7D%7D';
+  const params = semver.lt(grafanaVersion, '10.0.0')
+    ? 'left=%7B"datasource":"grafana","queries":%5B%7B"queryType":"randomWalk","refId":"A","datasource":%7B"type":"datasource","uid":"grafana"%7D%7D%5D,"range":%7B"from":"1547161200000","to":"1576364400000"%7D%7D&orgId=1'
+    : `panes=%7B"_t4":%7B"datasource":"grafana","queries":%5B%7B"queryType":"randomWalk","refId":"A","datasource":%7B"type":"datasource","uid":"grafana"%7D%7D%5D,"range":%7B"from":"now-6h","to":"now"%7D%7D%7D&orgId=1&left=%7B"datasource":"grafana","queries":%5B%7B"refId":"A","datasource":%7B"type":"datasource","uid":"grafana"%7D,"queryType":"randomWalk"%7D%5D,"range":%7B"from":"now-1h","to":"now"%7D%7D`;
 
-  await explorePage.goto({
-    queryParams: new URLSearchParams(url),
-  });
+  await explorePage.goto({ queryParams: new URLSearchParams(params) });
 
   await expect(explorePage.timeSeriesPanel.locator).toBeVisible();
   await expect(explorePage.tablePanel.locator).toBeVisible();
