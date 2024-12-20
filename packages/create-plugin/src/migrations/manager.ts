4ed388031ff3f56b4f5264b53c2d2320fdd873be
Levente Balogh <balogh.levente.hu@gmail.com>
2024-12-20 15:55:35 +0100

    Wip

--- packages/create-plugin/src/migrations/manager.ts
+++ packages/create-plugin/src/migrations/manager.ts
@@ -42,26 +42,84 @@ export function getMigrationsToRun(
   return migrationsToRun;
 }
 
+type ContextFile = Record<
+  string,
+  {
+    // The new content of the file
+    content: string;
+    // If `true` then it will delete the file regardless of the content
+    deleted?: true;
+  }
+>;
+
 class Context {
-  private files = {
-    'src/index.ts': {
-      originalContent: 'export const foo = 1;',
-    },
-  };
+  private files: ContextFile = {};
+
+  constructor() {}
+
+  // Adds a new file to the plugins
+  // `filePath` is always relative to the plugin root
+  addFile(filePath: string, content: string) {
+    if (!(await this.doesHavePermissionToWrite(filePath))) {
+    }
+
+    this.files[filePath] = { content };
+    // Error if no permission to write
+  }
+
+  // `filePath` is always relative to the plugin root
+  // Errors out if the file does not exist
+  deleteFile(filePath: string) {
+    if (!(await this.doesHavePermissionToWrite(filePath))) {
+    }
+
+    this.files[filePath] = { ...this.files[filePath], deleted: true };
+  }
+
+  // Applies update to a file in the plugins source code
+  updateFile(filePath: string, content: string) {
+    if (!(await this.doesHavePermissionToWrite(filePath))) {
+    }
+
+    this.files[filePath] = { content };
+  }
+
+  async doesFileExist(filePath: string) {
+    // check if a file with filepath exists on the disk
+  }
+
+  async doesHavePermissionToWrite(filePath: string) {}
+
+  // Searches a file based on a path in the plugins source code
+  async getFile(filePath: string) {
+    if (!(await this.doesFileExist(filePath))) {
+      // log error
+      return;
+    }
+
+    const contents = fs.readFileSync(filePath, 'utf-8');
+
+    return contents;
+  }
 }
 
-for (migration of migrationsToRun) {
-  try {
-    const changes = await runMigration({
-      version: migration.version,
-      description: migration.description,
-      migrationScript: migration.migrationScript,
-    });
+export async function runMigrations(migrations: MigrationMeta[]) {
+  for (const migration of migrations) {
+    try {
+      const context = await runMigration(migration, new Context());
 
-  } catch (error) {}
+      // logDiffForMigration(migration, context);
+      // applyChangesForMigration(migration, context);
+    } catch (error) {
+      // Log the error
+    }
+  }
+
+  // logChngesSummary(context);
+  // applyChangesToFiles(context);
 }
 
-export async function runMigration(migration: MigrationMeta): void {
+export async function runMigration(migration: MigrationMeta, context: Context): Context {
   // Run the migration script
   console.log(`Running migration: ${migration.description}`);
 }
