03d117eb50e2e0671993027d3512b2f11f302721
Erik Sundell <erik.sundell87@gmail.com>
2024-12-20 15:08:26 +0100

    handle special case for scenes

--- packages/plugin-e2e/src/fixtures/isFeatureToggleEnabled.ts
+++ packages/plugin-e2e/src/fixtures/isFeatureToggleEnabled.ts
@@ -1,4 +1,4 @@
-import { TestFixture } from '@playwright/test';
+import { Page, TestFixture } from '@playwright/test';
 import { PlaywrightArgs } from '../types';
 
 type FeatureToggleFixture = TestFixture<<T = object>(featureToggle: keyof T) => Promise<boolean>, PlaywrightArgs>;
@@ -8,4 +8,9 @@ export const isFeatureToggleEnabled: FeatureToggleFixture = async ({ page }, use
     const featureToggles: T = await page.evaluate('window.grafanaBootData.settings.featureToggles');
     return Boolean(featureToggles[featureToggle]);
   });
+};
+
+export const isFeatureEnabled = async (page: Page, featureToggle: string) => {
+  const featureToggles: Record<string, string> = await page.evaluate('window.grafanaBootData.settings.featureToggles');
+  return Boolean(featureToggles[featureToggle]);
 };
