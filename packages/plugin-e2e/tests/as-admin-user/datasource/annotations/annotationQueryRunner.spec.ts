18f95d0312abce13951426516ad6278c95aae532
Erik Sundell <erik.sundell87@gmail.com>
2024-12-20 15:09:12 +0100

    update annotation tests

---
+++ packages/plugin-e2e/tests/as-admin-user/datasource/annotations/annotationQueryRunner.spec.ts
@@ -0,0 +1,46 @@
+import * as semver from 'semver';
+import semverLt from 'semver/functions/lt';
+import { test, expect } from '../../../../src';
+
+test('create new, successful annotation query', async ({
+  grafanaVersion,
+  annotationEditPage,
+  readProvisionedDataSource,
+  page,
+}) => {
+  const ds = await readProvisionedDataSource({ fileName: 'testdatasource.yaml' });
+  await annotationEditPage.datasource.set(ds.name);
+  await page.getByRole('textbox', { name: 'Query Text' }).fill('annotationQuery');
+  await expect(annotationEditPage.runQuery()).toBeOK();
+  if (semver.gte(grafanaVersion, '11.0.0')) {
+    await expect(annotationEditPage).toHaveAlert('success');
+  }
+});
+
+test('create new, unsuccessful annotation query', async ({
+  grafanaVersion,
+  annotationEditPage,
+  readProvisionedDataSource,
+  page,
+}) => {
+  const ds = await readProvisionedDataSource({ fileName: 'testdatasource.yaml' });
+  await annotationEditPage.datasource.set(ds.name);
+  await page.getByRole('textbox', { name: 'Query Text' }).fill('error');
+  await expect(annotationEditPage.runQuery()).not.toBeOK();
+  if (semver.gte(grafanaVersion, '11.0.0')) {
+    await expect(annotationEditPage).toHaveAlert('error');
+  }
+});
+
+test('open provisioned, successful annotation query', async ({
+  grafanaVersion,
+  gotoAnnotationEditPage,
+  readProvisionedDashboard,
+}) => {
+  const dashboard = await readProvisionedDashboard({ fileName: 'testdatasource-annotations.json' });
+  const annotationEditPage = await gotoAnnotationEditPage({ dashboard, id: '1' });
+  await expect(annotationEditPage.runQuery()).toBeOK();
+  if (semver.gte(grafanaVersion, '11.0.0')) {
+    await expect(annotationEditPage).toHaveAlert('success');
+  }
+});
