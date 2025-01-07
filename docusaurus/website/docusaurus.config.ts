ccbe6baa8c5c45642027e2f2aedb632dad49c705
Jack Westbrook <jack.westbrook@gmail.com>
2025-01-07 12:57:24 +0100

    refactor(website): replace multiple ts docusaurus configs with a single ts file and env files

--- docusaurus/website/docusaurus.config.ts
+++ docusaurus/website/docusaurus.config.ts
@@ -1,35 +1,105 @@
-import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
-import {
-  customFields,
-  generalConfig,
-  plugins,
-  presetsDocs,
-  presetsTheme,
-  themeConfigNavbar,
-  themeConfigFooter,
-  themeConfigPrism,
-  themeConfigColorMode,
-} from './docusaurus.config.base';
+import type { Config } from '@docusaurus/types';
+import dotenv from 'dotenv';
+import remarkFigureCaption from 'gridsome-remark-figure-caption';
+import { resolve } from 'node:path';
+import { themes, type PrismTheme } from 'prism-react-renderer';
+import redirects from './websiteRedirects.json';
 
-const devPortalHome = 'https://grafana.com/developers';
-const [docsFooterLinks, ...otherFooterLinks] = themeConfigFooter.links;
 const gitHash = process.env.GITHUB_SHA || 'local';
+const envFile = process.env.DEV_PORTAL_ENV === 'dev' ? '.env.development' : '.env.production';
+
+dotenv.config({ path: `${__dirname}/${envFile}`, encoding: 'utf8' });
+
+const PORTAL_URL = `https://${process.env.DEV_PORTAL_HOST}`;
+const PORTAL_HOME_HREF = `${PORTAL_URL}/developers`;
+
+// Replace background and color to better match Grafana theme.
+const grafanaPrismTheme: PrismTheme = {
+  ...themes.oneDark,
+  plain: {
+    color: 'rgb(204, 204, 220)',
+    backgroundColor: '#181b1f',
+  },
+};
 
 const config: Config = {
-  ...generalConfig,
-  url: 'https://grafana.com/',
+  title: 'Grafana Plugin Tools',
+  tagline: 'Scaffold a Grafana plugin with one command',
+  url: PORTAL_URL,
   baseUrl: 'developers/plugin-tools/',
-  plugins,
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.png',
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'grafana', // Usually your GitHub org/user name.
+  projectName: 'plugin-tools', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  future: {
+    experimental_faster: true,
+  },
+  plugins: [
+    [
+      './plugins/docusaurus-custom-webpack-plugin',
+      {
+        resolve: {
+          alias: {
+            '@snippets': resolve(__dirname, '..', 'docs', 'snippets'),
+            '@shared': resolve(__dirname, '..', 'docs', 'shared'),
+          },
+        },
+      },
+    ],
+    [
+      'docusaurus-lunr-search',
+      {
+        disableVersioning: true,
+      },
+    ],
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        // how to use https://docusaurus.io/docs/api/plugins/@docusaurus/plugin-client-redirects#ex-config
+        // This plugin is only active in production because it works on the build output.
+        // Edit the redirects in file://./websiteRedirects.json
+        redirects,
+      },
+    ],
+  ],
   presets: [
     [
       'classic',
       {
         docs: {
-          ...presetsDocs,
+          path: '../docs',
+          exclude: ['**/snippets/**', '**/shared/**', '**/drafts/**'],
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/grafana/plugin-tools/edit/main/docusaurus/website',
+          beforeDefaultRemarkPlugins: [
+            [
+              remarkFigureCaption,
+              {
+                figureClassName: 'md-figure-block',
+                imageClassName: 'md-figure-image',
+                captionClassName: 'md-figure-caption',
+              },
+            ],
+          ],
           routeBasePath: '/',
         },
-        theme: presetsTheme,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
         blog: false,
       } satisfies Preset.Options,
     ],
@@ -45,52 +115,104 @@ const config: Config = {
       isCloseable: false,
     },
     navbar: {
-      ...themeConfigNavbar,
+      title: 'Grafana Plugin Tools',
+      logo: {
+        alt: 'Grafana Logo',
+        src: 'img/logo.svg',
+      },
       items: [
-        { href: devPortalHome, label: 'Portal Home', position: 'right', target: '_self' },
-        ...themeConfigNavbar.items,
+        { href: PORTAL_HOME_HREF, label: 'Portal Home', position: 'right', target: '_self' },
+        { href: 'https://community.grafana.com/c/plugin-development/30', label: 'Help', position: 'right' },
+        {
+          href: 'https://www.github.com/grafana/plugin-tools',
+          label: 'GitHub',
+          position: 'right',
+        },
       ],
     },
     footer: {
-      ...themeConfigFooter,
+      style: 'dark',
       links: [
         {
-          ...docsFooterLinks,
+          title: 'Docs',
           items: [
-            ...docsFooterLinks.items,
+            {
+              label: 'Get Started',
+              to: '/',
+            },
             {
               label: 'Portal Home',
-              href: devPortalHome,
+              href: PORTAL_HOME_HREF,
               target: '_self',
             },
           ],
         },
-        ...otherFooterLinks,
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/grafana',
+            },
+            {
+              label: 'Github Discussions',
+              href: 'https://www.github.com/grafana/plugin-tools/discussions',
+            },
+            {
+              label: 'Grafana Community Forums',
+              href: 'https://community.grafana.com/c/plugin-development/30',
+            },
+          ],
+        },
+        {
+          title: 'Social',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://www.github.com/grafana/plugin-tools',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Grafana Labs. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: grafanaPrismTheme,
+      additionalLanguages: ['bash', 'diff', 'json'],
+      magicComments: [
+        {
+          className: 'code-block-addition-highlighted-line',
+          line: 'addition-highlight-next-line',
+          block: { start: 'addition-highlight-start', end: 'addition-highlight-end' },
+        },
       ],
     },
-    prism: themeConfigPrism,
-    colorMode: themeConfigColorMode,
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
+    },
   },
 
   customFields: {
-    ...customFields,
+    nodeEnv: process.env.NODE_ENV,
     rudderStackTracking: {
-      url: 'https://rs.grafana.com',
-      writeKey: '1sBAgwTlZ2K0zTzkM8YTWorZI00',
-      configUrl: 'https://rsc.grafana.com',
-      sdkUrl: 'https://rsdk.grafana.com',
+      url: process.env.DEV_PORTAL_RUDDERSTACK_URL,
+      writeKey: process.env.DEV_PORTAL_RUDDERSTACK_WRITE_KEY,
+      configUrl: process.env.DEV_PORTAL_RUDDERSTACK_CONFIG_URL,
+      sdkUrl: process.env.DEV_PORTAL_RUDDERSTACK_SDK_URL,
     },
-    canSpamUrl: 'https://grafana.com/canspam',
+    canSpamUrl: `${PORTAL_URL}/canspam`,
     faroConfig: {
-      url: '/connect/af1fca71911a9641ebdffddb56889e97',
-      appName: 'grafana-website',
+      url: process.env.DEV_PORTAL_FARO_CONFIG_URL,
+      appName: process.env.DEV_PORTAL_FARO_CONFIG_APP_NAME,
       version: gitHash,
-      environment: 'production',
+      environment: process.env.DEV_PORTAL_FARO_CONFIG_ENVIRONMENT,
     },
   },
   scripts: [
     // adobe target
-    'https://grafana.com/at.js',
+    `${PORTAL_URL}/at.js`,
   ],
 };
 
