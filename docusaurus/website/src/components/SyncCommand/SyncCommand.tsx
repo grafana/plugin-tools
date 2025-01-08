48f76616aea9514075bc16b786ed1beb95e860c8
Jack Westbrook <jack.westbrook@gmail.com>
2025-01-08 12:54:54 +0100

    feat(website): syncCommand component work with npm2yarn plugin

--- docusaurus/website/src/components/SyncCommand/SyncCommand.tsx
+++ docusaurus/website/src/components/SyncCommand/SyncCommand.tsx
@@ -7,17 +7,8 @@ interface SyncCommandProps {
 }
 
 function BrowserSyncCommand({ cmd }: SyncCommandProps) {
-  let currentPackageManager = window.localStorage['docusaurus.tab.package-manager'];
-  const location = useLocation();
+  let currentPackageManager = window.localStorage['docusaurus.tab.npm2yarn'];
 
-  if (!currentPackageManager) {
-    if (location && location.search && location.search.includes('current-package-manager')) {
-      const searchParams = new URLSearchParams(location.search);
-      currentPackageManager = searchParams.get('current-package-manager');
-    } else {
-      currentPackageManager = 'npm';
-    }
-  }
   const cmdString = `${currentPackageManager} ${cmd}`;
 
   return <code>{cmdString}</code>;
