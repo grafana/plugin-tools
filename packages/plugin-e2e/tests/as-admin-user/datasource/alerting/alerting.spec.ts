bbe402ce1bcee66b07363d9bd2c58c2bdb56a817
Erik Sundell <erik.sundell87@gmail.com>
2025-01-27 10:08:39 +0100

    temp disable test

--- packages/plugin-e2e/tests/as-admin-user/datasource/alerting/alerting.spec.ts
+++ packages/plugin-e2e/tests/as-admin-user/datasource/alerting/alerting.spec.ts
@@ -48,7 +48,6 @@ test.describe('Test new alert rules', () => {
     const queryA = alertRuleEditPage.getAlertRuleQueryRow('A');
     await queryA.datasource.set(ds.name);
     const rowCount = await alertRuleEditPage.getByGrafanaSelector(rows).count();
-    semver.gte(grafanaVersion, '11.5.0') && (await page.getByLabel('Advanced options').nth(1).check());
     await alertRuleEditPage.clickAddQueryRow();
     await expect(alertRuleEditPage.getByGrafanaSelector(rows)).toHaveCount(rowCount + 1);
     await alertRuleEditPage.clickAddQueryRow();
