2fbe106c4bb885aaf1c2f1bc98bc18ec027e488e
Jack Westbrook <jack.westbrook@gmail.com>
2025-02-19 09:35:29 +0100

    feat(create-plugin): remove development scripts and code related to generated directory

--- packages/create-plugin/src/utils/utils.path.ts
+++ packages/create-plugin/src/utils/utils.path.ts
@@ -1,11 +1,7 @@
 import path from 'node:path';
-import { PLUGIN_TYPES, IS_DEV, DEV_EXPORT_DIR } from '../constants.js';
+import { PLUGIN_TYPES } from '../constants.js';
 import { normalizeId } from './utils.handlebars.js';
 
-export function getExportPath(pluginName: string, orgName: string, pluginType: PLUGIN_TYPES): string {
-  if (IS_DEV) {
-    return DEV_EXPORT_DIR;
-  } else {
-    return path.join(process.cwd(), normalizeId(pluginName, orgName, pluginType));
-  }
+export function getExportPath(pluginName: string, orgName: string, pluginType: PLUGIN_TYPES) {
+  return path.join(process.cwd(), normalizeId(pluginName, orgName, pluginType));
 }
