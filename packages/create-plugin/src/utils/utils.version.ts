d105bb8e9d27733e93bf9fff039b40674415ad16
Jack Westbrook <jack.westbrook@gmail.com>
2025-03-04 16:47:04 +0100

    feat(create-plugin): use libs/version for app version

--- packages/create-plugin/src/utils/utils.version.ts
+++ packages/create-plugin/src/utils/utils.version.ts
@@ -1,23 +1,9 @@
-import { findUpSync } from 'find-up';
 import { readFileSync } from 'node:fs';
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { getVersion } from '@libs/version';
 import { TEMPLATE_PATHS } from '../constants.js';
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
-
-export function getVersion(): string {
-  const packageJsonPath = findUpSync('package.json', { cwd: __dirname });
-  if (!packageJsonPath) {
-    throw `Could not find package.json`;
-  }
-  const pkg = readFileSync(packageJsonPath, 'utf8');
-  const { version } = JSON.parse(pkg);
-  if (!version) {
-    throw `Could not find the version of create-plugin`;
-  }
-  return version;
-}
+export const CURRENT_APP_VERSION = getVersion();
 
 export function getGrafanaRuntimeVersion() {
   const packageJsonPath = path.join(TEMPLATE_PATHS.common, '_package.json');
