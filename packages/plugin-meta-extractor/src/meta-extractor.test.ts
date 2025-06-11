f57c496f8fe8db2142ab9cdb37f025f7dc39ee78
Marcus Andersson <marcus.andersson@grafana.com>
2025-06-11 07:27:10 +0200

    Chore: Disabling tests for the meta extractor (#1875)

--- packages/plugin-meta-extractor/src/meta-extractor.test.ts
+++ packages/plugin-meta-extractor/src/meta-extractor.test.ts
@@ -5,71 +5,74 @@ const fixturesPath = `${__dirname}/../fixtures`;
 const defaultExportFixtures = `${fixturesPath}/defaultExport`;
 const namedExportFixtures = `${fixturesPath}/namedExport`;
 
-describe('When app is exported as a NAMED-EXPORT', () => {
-  describe('and extensions registered by chaining function calls', () => {
-    it('should return expected meta from app plugin', () => {
-      const entry = `${namedExportFixtures}/chained.tsx`;
-      const extensionPoints = extractPluginExtensions(entry);
+// Skip the test since this is an experimental package that doesn't support the latest version of plugins.
+describe.skip('When extracting meta form plugins', () => {
+  describe('When app is exported as a NAMED-EXPORT', () => {
+    describe('and extensions registered by chaining function calls', () => {
+      it('should return expected meta from app plugin', () => {
+        const entry = `${namedExportFixtures}/chained.tsx`;
+        const extensionPoints = extractPluginExtensions(entry);
 
-      expect(extensionPoints).toHaveLength(4);
-      expect(extensionPoints).toContainEqual({
-        type: PluginExtensionTypes.component,
-        extensionPointId: 'grafana/commandpalette/action',
-        title: 'Component title 1',
-        description: 'Component description 1',
-      });
-      expect(extensionPoints).toContainEqual({
-        type: PluginExtensionTypes.link,
-        extensionPointId: 'grafana/dashboard/panel/menu',
-        title: 'Link title 1',
-        description: 'Link description 1',
-      });
-      expect(extensionPoints).toContainEqual({
-        type: PluginExtensionTypes.component,
-        extensionPointId: 'grafana/dashboard/panel/menu',
-        title: 'Component title 2',
-        description: 'Component description 2',
-      });
-      expect(extensionPoints).toContainEqual({
-        type: PluginExtensionTypes.link,
-        extensionPointId: 'grafana/commandpalette/action',
-        title: 'Link title 2',
-        description: 'Link description 2',
+        expect(extensionPoints).toHaveLength(4);
+        expect(extensionPoints).toContainEqual({
+          type: PluginExtensionTypes.component,
+          extensionPointId: 'grafana/commandpalette/action',
+          title: 'Component title 1',
+          description: 'Component description 1',
+        });
+        expect(extensionPoints).toContainEqual({
+          type: PluginExtensionTypes.link,
+          extensionPointId: 'grafana/dashboard/panel/menu',
+          title: 'Link title 1',
+          description: 'Link description 1',
+        });
+        expect(extensionPoints).toContainEqual({
+          type: PluginExtensionTypes.component,
+          extensionPointId: 'grafana/dashboard/panel/menu',
+          title: 'Component title 2',
+          description: 'Component description 2',
+        });
+        expect(extensionPoints).toContainEqual({
+          type: PluginExtensionTypes.link,
+          extensionPointId: 'grafana/commandpalette/action',
+          title: 'Link title 2',
+          description: 'Link description 2',
+        });
       });
     });
   });
-});
 
-describe('When app is exported as a DEFAULT-EXPORT', () => {
-  describe('and extensions registered by chaining function calls', () => {
-    it('should return expected meta from app plugin', () => {
-      const entry = `${defaultExportFixtures}/chained.tsx`;
-      const extensionPoints = extractPluginExtensions(entry);
+  describe('When app is exported as a DEFAULT-EXPORT', () => {
+    describe('and extensions registered by chaining function calls', () => {
+      it('should return expected meta from app plugin', () => {
+        const entry = `${defaultExportFixtures}/chained.tsx`;
+        const extensionPoints = extractPluginExtensions(entry);
 
-      expect(extensionPoints).toHaveLength(4);
-      expect(extensionPoints).toContainEqual({
-        type: PluginExtensionTypes.component,
-        extensionPointId: 'grafana/commandpalette/action',
-        title: 'Component title 1',
-        description: 'Component description 1',
-      });
-      expect(extensionPoints).toContainEqual({
-        type: PluginExtensionTypes.link,
-        extensionPointId: 'grafana/dashboard/panel/menu',
-        title: 'Link title 1',
-        description: 'Link description 1',
-      });
-      expect(extensionPoints).toContainEqual({
-        type: PluginExtensionTypes.component,
-        extensionPointId: 'grafana/dashboard/panel/menu',
-        title: 'Component title 2',
-        description: 'Component description 2',
-      });
-      expect(extensionPoints).toContainEqual({
-        type: PluginExtensionTypes.link,
-        extensionPointId: 'grafana/commandpalette/action',
-        title: 'Link title 2',
-        description: 'Link description 2',
+        expect(extensionPoints).toHaveLength(4);
+        expect(extensionPoints).toContainEqual({
+          type: PluginExtensionTypes.component,
+          extensionPointId: 'grafana/commandpalette/action',
+          title: 'Component title 1',
+          description: 'Component description 1',
+        });
+        expect(extensionPoints).toContainEqual({
+          type: PluginExtensionTypes.link,
+          extensionPointId: 'grafana/dashboard/panel/menu',
+          title: 'Link title 1',
+          description: 'Link description 1',
+        });
+        expect(extensionPoints).toContainEqual({
+          type: PluginExtensionTypes.component,
+          extensionPointId: 'grafana/dashboard/panel/menu',
+          title: 'Component title 2',
+          description: 'Component description 2',
+        });
+        expect(extensionPoints).toContainEqual({
+          type: PluginExtensionTypes.link,
+          extensionPointId: 'grafana/commandpalette/action',
+          title: 'Link title 2',
+          description: 'Link description 2',
+        });
       });
     });
   });
