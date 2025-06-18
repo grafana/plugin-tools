0b990d0cbaf8eb8b24252057a35b13a5be00e38e
Esteban Beltran <sergies@gmail.com>
2025-06-18 17:14:16 +0200

    add migration to migrations

--- packages/create-plugin/src/migrations/migrations.ts
+++ packages/create-plugin/src/migrations/migrations.ts
@@ -21,5 +21,11 @@ export default {
       description: 'Update ./docker-compose.yaml to extend from ./.config/docker-compose-base.yaml.',
       migrationScript: './scripts/001-update-grafana-compose-extend.js',
     },
+    '002-update-is-compatible-workflow': {
+      version: '5.23.2',
+      description:
+        'Update ./.github/workflows/is-compatible.yml to use is-compatible github action instead of calling levitate directly',
+      migrationScript: './scripts/002-update-is-compatible-workflow.js',
+    },
   },
 } as Migrations;
