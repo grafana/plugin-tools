56c18751b9e46f4e77c6c40c37685461cead509b
Jack Westbrook <jack.westbrook@gmail.com>
2025-08-07 14:14:33 +0200

    test(plugin-e2e): update goto test as we no longer use networkIdle as default

--- packages/plugin-e2e/tests/as-admin-user/page-loading/goto.spec.ts
+++ packages/plugin-e2e/tests/as-admin-user/page-loading/goto.spec.ts
@@ -1,40 +1,22 @@
 import { test, expect } from '../../../src';
 
 test.describe('gotoDashboardPage', () => {
-  test('should not display elements when waitUntil `load` is used', async ({
+  test('should display elements when waitUntil `load` (default) is used', async ({
     gotoDashboardPage,
     readProvisionedDashboard,
   }) => {
     const dashboard = await readProvisionedDashboard({ fileName: 'testdatasource.json' });
-    const dashboardPage = await gotoDashboardPage({ ...dashboard, waitUntil: 'load' });
-    await expect(dashboardPage.getPanelByTitle('Table data').locator).toHaveCount(0);
-  });
-
-  test('should not display elements when waitUntil `networkidle` (default) is used', async ({
-    gotoDashboardPage,
-    readProvisionedDashboard,
-  }) => {
-    const dashboard = await readProvisionedDashboard({ fileName: 'testdatasource.json' });
-    const dashboardPage = await gotoDashboardPage(dashboard);
+    const dashboardPage = await gotoDashboardPage({ ...dashboard });
     await expect(dashboardPage.getPanelByTitle('Table data').locator).toBeVisible();
   });
 });
 
 test.describe('gotoPanelEditPage', () => {
-  test('should not display elements when waitUntil `load` is used', async ({
+  test('should display elements when waitUntil `load` (default) is used', async ({
     gotoPanelEditPage,
     readProvisionedDashboard,
   }) => {
-    const dashboard = await readProvisionedDashboard({ fileName: 'testdatasource.json' });
-    const panelEditPage = await gotoPanelEditPage({ dashboard, id: '3', waitUntil: 'load' });
-    await expect(panelEditPage.panel.locator).toHaveCount(0);
-  });
-
-  test('should not display elements when waitUntil `networkidle` (default) is used', async ({
-    gotoPanelEditPage,
-    readProvisionedDashboard,
-  }) => {
-    const dashboard = await readProvisionedDashboard({ fileName: 'testdatasource.json' });
+    const dashboard = await readProvisionedDashboard({ fileName: 'testdatasource.jsonnpm' });
     const panelEditPage = await gotoPanelEditPage({ dashboard, id: '3' });
     await expect(panelEditPage.panel.locator).toBeVisible();
   });
