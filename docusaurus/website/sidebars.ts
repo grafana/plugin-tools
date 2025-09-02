d033178629b854ec66af548a83f24d92fd9b1e83
Levente Balogh <balogh.levente.hu@gmail.com>
2025-09-02 11:37:43 +0200

    docs: add how-to guide for function extensions

--- docusaurus/website/sidebars.ts
+++ docusaurus/website/sidebars.ts
@@ -146,8 +146,9 @@ const sidebars: SidebarsConfig = {
             id: 'how-to-guides/ui-extensions/ui-extensions',
           },
           items: [
-            'how-to-guides/ui-extensions/create-an-extension-point',        
-            'how-to-guides/ui-extensions/extension-user-render-component',   
+            'how-to-guides/ui-extensions/create-an-extension-point',
+            'how-to-guides/ui-extensions/extension-user-render-component',
+            'how-to-guides/ui-extensions/extension-user-use-function',
             'how-to-guides/ui-extensions/use-an-exposed-component',
             'how-to-guides/ui-extensions/register-an-extension',
             'how-to-guides/ui-extensions/expose-a-component',
@@ -297,8 +298,8 @@ const sidebars: SidebarsConfig = {
         id: 'reference/plugin-reference',
       },
       items: [
-        'reference/plugin-json', 
-        'reference/cli-commands', 
+        'reference/plugin-json',
+        'reference/cli-commands',
         {
           type: 'category',
           label: 'UI extensions reference',
@@ -308,9 +309,9 @@ const sidebars: SidebarsConfig = {
             id: 'reference/ui-extensions-reference/ui-extensions-reference',
           },
           items: [
-            'reference/ui-extensions-reference/extension-points',   
-            'reference/ui-extensions-reference/ui-extensions',   
-            'reference/ui-extensions-reference/extensions-api-deprecated',  
+            'reference/ui-extensions-reference/extension-points',
+            'reference/ui-extensions-reference/ui-extensions',
+            'reference/ui-extensions-reference/extensions-api-deprecated',
           ],
         },
       ],
