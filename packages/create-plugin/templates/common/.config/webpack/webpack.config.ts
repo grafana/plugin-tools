0980f0e579b3d17a0df25639d641f2a4309402ce
Jack Westbrook <jack.westbrook@gmail.com>
2025-09-05 11:07:10 +0200

    feat(create-plugin): externalise react/jsx-dev-runtime to prevent issues appearing only in prod

--- packages/create-plugin/templates/common/.config/webpack/webpack.config.ts
+++ packages/create-plugin/templates/common/.config/webpack/webpack.config.ts
@@ -69,6 +69,7 @@ const config = async (env: Env): Promise<Configuration> => {
       '@grafana/slate-react',
       'react',
       'react/jsx-runtime',
+      'react/jsx-dev-runtime',
       'react-dom',
       'react-redux',
       'redux',
