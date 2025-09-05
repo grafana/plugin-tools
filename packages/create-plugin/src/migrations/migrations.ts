98d45b6d91ef701af914c71224e7c1dfeba693e0
Jack Westbrook <jack.westbrook@gmail.com>
2025-09-05 16:12:58 +0200

    feat(create-plugin): force plugins to manually bump to last v5 release before running migrations

--- packages/create-plugin/src/migrations/migrations.ts
+++ packages/create-plugin/src/migrations/migrations.ts
@@ -1,3 +1,5 @@
+import { BASELINE_VERSION_FOR_MIGRATIONS } from '../constants.js';
+
 export type MigrationMeta = {
   version: string;
   description: string;
@@ -10,32 +12,27 @@ type Migrations = {
 
 export default {
   migrations: {
-    // Example migration entry (DO NOT UNCOMMENT!)
-    // 'example-migration': {
-    //   version: '5.13.0',
-    //   description: 'Update build command to use webpack profile flag.',
-    //   migrationScript: './scripts/example-migration.js',
-    // },
     '001-update-grafana-compose-extend': {
-      version: '5.19.2',
+      version: BASELINE_VERSION_FOR_MIGRATIONS,
       description: 'Update ./docker-compose.yaml to extend from ./.config/docker-compose-base.yaml.',
       migrationScript: './scripts/001-update-grafana-compose-extend.js',
     },
     '002-update-is-compatible-workflow': {
-      version: '5.24.0',
+      version: BASELINE_VERSION_FOR_MIGRATIONS,
       description:
         'Update ./.github/workflows/is-compatible.yml to use is-compatible github action instead of calling levitate directly',
       migrationScript: './scripts/002-update-is-compatible-workflow.js',
     },
     '003-update-eslint-deprecation-rule': {
-      version: '5.24.1',
+      version: BASELINE_VERSION_FOR_MIGRATIONS,
       description: 'Replace deprecated eslint-plugin-deprecation with @typescript-eslint/no-deprecated rule.',
       migrationScript: './scripts/003-update-eslint-deprecation-rule.js',
     },
     '004-eslint9-flat-config': {
-      version: '5.25.4',
+      version: BASELINE_VERSION_FOR_MIGRATIONS,
       description: 'Migrate eslint config to flat config format and update devDependencies to latest versions.',
       migrationScript: './scripts/004-eslint9-flat-config.js',
     },
+    // DO NOT USE BASELINE_VERSION_FOR_MIGRATIONS FOR NEW MIGRATIONS.
   },
 } as Migrations;
