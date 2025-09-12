ae64018a34889a0c1b23562850d65bb5bea51ce6
Jack Westbrook <jack.westbrook@gmail.com>
2025-09-12 17:41:27 +0200

    refactor(create-plugin): tidy up update command, var names, commands to run

--- packages/create-plugin/src/migrations/migrations.ts
+++ packages/create-plugin/src/migrations/migrations.ts
@@ -1,4 +1,4 @@
-import { BASELINE_VERSION_FOR_MIGRATIONS } from '../constants.js';
+import { LEGACY_UPDATE_CUTOFF_VERSION } from '../constants.js';
 
 export type MigrationMeta = {
   version: string;
@@ -13,26 +13,27 @@ type Migrations = {
 export default {
   migrations: {
     '001-update-grafana-compose-extend': {
-      version: BASELINE_VERSION_FOR_MIGRATIONS,
+      version: LEGACY_UPDATE_CUTOFF_VERSION,
       description: 'Update ./docker-compose.yaml to extend from ./.config/docker-compose-base.yaml.',
       migrationScript: './scripts/001-update-grafana-compose-extend.js',
     },
     '002-update-is-compatible-workflow': {
-      version: BASELINE_VERSION_FOR_MIGRATIONS,
+      version: LEGACY_UPDATE_CUTOFF_VERSION,
       description:
         'Update ./.github/workflows/is-compatible.yml to use is-compatible github action instead of calling levitate directly',
       migrationScript: './scripts/002-update-is-compatible-workflow.js',
     },
     '003-update-eslint-deprecation-rule': {
-      version: BASELINE_VERSION_FOR_MIGRATIONS,
+      version: LEGACY_UPDATE_CUTOFF_VERSION,
       description: 'Replace deprecated eslint-plugin-deprecation with @typescript-eslint/no-deprecated rule.',
       migrationScript: './scripts/003-update-eslint-deprecation-rule.js',
     },
     '004-eslint9-flat-config': {
-      version: BASELINE_VERSION_FOR_MIGRATIONS,
+      version: LEGACY_UPDATE_CUTOFF_VERSION,
       description: 'Migrate eslint config to flat config format and update devDependencies to latest versions.',
       migrationScript: './scripts/004-eslint9-flat-config.js',
     },
-    // DO NOT USE BASELINE_VERSION_FOR_MIGRATIONS FOR NEW MIGRATIONS.
+    // Do not use LEGACY_UPDATE_CUTOFF_VERSION for new migrations. It is only used above to force migrations to run
+    // for those written before the switch to updates as migrations.
   },
 } as Migrations;
