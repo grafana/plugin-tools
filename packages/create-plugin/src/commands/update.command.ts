5995f6957afb14cd0df0b08b85db9815155e623d
Jack Westbrook <jack.westbrook@gmail.com>
2025-09-12 17:41:27 +0200

    refactor(create-plugin): tidy up update command, var names, commands to run

--- packages/create-plugin/src/commands/update.command.ts
+++ packages/create-plugin/src/commands/update.command.ts
@@ -1,19 +1,50 @@
 import minimist from 'minimist';
 import { gte, lt } from 'semver';
-import { gitCommitNoVerify, isGitDirectory, isGitDirectoryClean } from '../utils/utils.git.js';
+import { isGitDirectory, isGitDirectoryClean } from '../utils/utils.git.js';
 import { getConfig } from '../utils/utils.config.js';
 import { output } from '../utils/utils.console.js';
 import { isPluginDirectory } from '../utils/utils.plugin.js';
 import { getPackageManagerExecCmd, getPackageManagerWithFallback } from '../utils/utils.packageManager.js';
-import { BASELINE_VERSION_FOR_MIGRATIONS } from '../constants.js';
-import { exec as nodeExec } from 'node:child_process';
-import { promisify } from 'node:util';
+import { LEGACY_UPDATE_CUTOFF_VERSION } from '../constants.js';
+import { spawnSync } from 'node:child_process';
 import { getMigrationsToRun, runMigrations } from '../migrations/manager.js';
 import { CURRENT_APP_VERSION } from '../utils/utils.version.js';
 
-const exec = promisify(nodeExec);
-
 export const update = async (argv: minimist.ParsedArgs) => {
+  performPreUpdateChecks(argv);
+  const { version } = getConfig();
+
+  if (lt(version, LEGACY_UPDATE_CUTOFF_VERSION)) {
+    preparePluginForMigrations(argv);
+  }
+
+  try {
+    if (gte(version, CURRENT_APP_VERSION)) {
+      output.log({
+        title: 'Nothing to update, exiting.',
+      });
+
+      process.exit(0);
+    }
+
+    const commitEachMigration = argv.commit;
+    const migrations = getMigrationsToRun(version, CURRENT_APP_VERSION);
+    await runMigrations(migrations, { commitEachMigration });
+    output.success({
+      title: `Successfully updated create-plugin from ${version} to ${CURRENT_APP_VERSION}.`,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      output.error({
+        title: 'Update failed',
+        body: [error.message],
+      });
+    }
+    process.exit(1);
+  }
+};
+
+async function performPreUpdateChecks(argv: minimist.ParsedArgs) {
   if (!(await isGitDirectory()) && !argv.force) {
     output.error({
       title: 'You are not inside a git directory',
@@ -53,73 +84,53 @@ export const update = async (argv: minimist.ParsedArgs) => {
 
     process.exit(1);
   }
+}
 
-  const config = getConfig();
+function preparePluginForMigrations(argv: minimist.ParsedArgs) {
+  const { packageManagerName, packageManagerVersion } = getPackageManagerWithFallback();
+  const packageManagerExecCmd = getPackageManagerExecCmd(packageManagerName, packageManagerVersion);
 
-  if (lt(config.version, BASELINE_VERSION_FOR_MIGRATIONS)) {
-    const { packageManagerName, packageManagerVersion } = getPackageManagerWithFallback();
-    const packageManagerExecCmd = getPackageManagerExecCmd(packageManagerName, packageManagerVersion);
+  const updateCmdList = [
+    `${packageManagerExecCmd}@${LEGACY_UPDATE_CUTOFF_VERSION} update`,
+    `${packageManagerName} install`,
+  ];
+  const gitCmdList = [
+    'git add -A',
+    `git commit -m 'chore: run create-plugin@${LEGACY_UPDATE_CUTOFF_VERSION} update' --no-verify`,
+  ];
 
-    const cmdList = [
-      `${output.formatCode(`${packageManagerExecCmd}@${BASELINE_VERSION_FOR_MIGRATIONS} update`)}`,
-      `${output.formatCode(`${packageManagerName} install`)}`,
-      `${output.formatCode('git add -A')}`,
-      `${output.formatCode(`git commit -m "chore: run create-plugin@${BASELINE_VERSION_FOR_MIGRATIONS} update"`)}`,
-    ];
-
-    try {
-      output.warning({
-        title: `Update to create-plugin ${BASELINE_VERSION_FOR_MIGRATIONS} required.`,
-        body: ['The following commands will be run before updating your plugin to create-plugin v6+.', ...cmdList],
-      });
-
-      await exec(`${packageManagerExecCmd}@${BASELINE_VERSION_FOR_MIGRATIONS} update`);
-      await exec(`${packageManagerName} install`);
-      await exec('git add -A');
-      await gitCommitNoVerify(`chore: run create-plugin@${BASELINE_VERSION_FOR_MIGRATIONS} update`);
-    } catch (error) {
-      output.error({
-        title: `Update to create-plugin ${BASELINE_VERSION_FOR_MIGRATIONS} failed.`,
-        body: [
-          'Please run the following commands manually and try again.',
-          ...cmdList,
-          'error:',
-          error instanceof Error ? error.message : String(error),
-        ],
-      });
-      process.exit(1);
-    }
-  }
-
-  return await migrationUpdate(argv);
-};
-
-const migrationUpdate = async (argv: minimist.ParsedArgs) => {
   try {
-    const projectCpVersion = getConfig().version;
-    const packageCpVersion = CURRENT_APP_VERSION;
-
-    if (gte(projectCpVersion, packageCpVersion)) {
-      output.log({
-        title: 'Nothing to update, exiting.',
-      });
-
-      process.exit(0);
-    }
-
-    const commitEachMigration = argv.commit;
-    const migrations = getMigrationsToRun(projectCpVersion, packageCpVersion);
-    await runMigrations(migrations, { commitEachMigration });
-    output.success({
-      title: `Successfully updated create-plugin from ${projectCpVersion} to ${packageCpVersion}.`,
+    output.warning({
+      title: `Update to create-plugin ${LEGACY_UPDATE_CUTOFF_VERSION} required.`,
+      body: ['Running additional commands before updating your plugin to create-plugin v6+.'],
     });
-  } catch (error) {
-    if (error instanceof Error) {
-      output.error({
-        title: 'Update failed',
-        body: [error.message],
+
+    for (const cmd of updateCmdList) {
+      output.log({
+        title: `Running ${output.formatCode(cmd)}`,
       });
+      spawnSync(cmd, { shell: true, stdio: 'inherit', cwd: process.cwd() });
     }
+
+    if (argv.commit) {
+      for (const cmd of gitCmdList) {
+        output.log({
+          title: `Running ${output.formatCode(cmd)}`,
+        });
+        spawnSync(cmd, { shell: true, cwd: process.cwd() });
+      }
+    }
+  } catch (error) {
+    output.error({
+      title: `Update to create-plugin ${LEGACY_UPDATE_CUTOFF_VERSION} failed.`,
+      body: [
+        'Please run the following commands manually and try again.',
+        ...updateCmdList,
+        ...(argv.commit ? gitCmdList : []),
+        'error:',
+        error instanceof Error ? error.message : String(error),
+      ],
+    });
     process.exit(1);
   }
-};
+}
