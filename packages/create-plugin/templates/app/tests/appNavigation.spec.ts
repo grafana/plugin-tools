b7e4df34bc545185c5c2f9a1e06601c2ae204017
Hugo Häggmark <hugo.haggmark@gmail.com>
2025-09-23 07:04:38 +0200

    chore: fix failing e2e tests

--- packages/create-plugin/templates/app/tests/appNavigation.spec.ts
+++ packages/create-plugin/templates/app/tests/appNavigation.spec.ts
@@ -1,25 +1,25 @@
 import { test, expect } from './fixtures';
-import { ROUTES } from '../src/constants';
+import { PluginIncludeRelativePaths } from '../src/codegen/includes';
 
 test.describe('navigating app', () => {
   test('page one should render successfully', async ({ gotoPage, page }) => {
-    await gotoPage(`/${ROUTES.One}`);
+    await gotoPage(`/${PluginIncludeRelativePaths.PageOne}`);
     await expect(page.getByText('This is page one.')).toBeVisible();
   });
 
   test('page two should render successfully', async ({ gotoPage, page }) => {
-    await gotoPage(`/${ROUTES.Two}`);
+    await gotoPage(`/${PluginIncludeRelativePaths.PageTwo}`);
     await expect(page.getByText('This is page two.')).toBeVisible();
   });
 
   test('page three should support an id parameter', async ({ gotoPage, page }) => {
-    await gotoPage(`/${ROUTES.Three}/123456`);
+    await gotoPage(`/${PluginIncludeRelativePaths.PageThree}/123456`);
     await expect(page.getByText('ID: 123456')).toBeVisible();
   });
 
-  test('page three should render sucessfully', async ({ gotoPage, page }) => {
+  test('page four should render sucessfully', async ({ gotoPage, page }) => {
     // wait for page to successfully render
-    await gotoPage(`/${ROUTES.One}`);
+    await gotoPage(`/${PluginIncludeRelativePaths.PageOne}`);
     await expect(page.getByText('This is page one.')).toBeVisible();
 
     // navigating to page four with full width layout without sidebar menu
