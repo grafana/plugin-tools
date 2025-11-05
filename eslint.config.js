fa9904b61e691bae466a2f41e24f881683bf86a5
Jack Westbrook <jack.westbrook@gmail.com>
2025-11-05 09:31:57 +0100

    docs(website): add eslint-plugin-mdx and update eslint config to lint our docs

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 // @ts-check
 const grafanaConfig = require('@grafana/eslint-config/flat');
+const mdxParser = require('eslint-mdx');
+const mdxPlugin = require('eslint-plugin-mdx');
 
 // If you edit this config consider running `npx -y @eslint/config-inspector@latest` first.
 
@@ -15,13 +17,17 @@ module.exports = [
       '**/.*', // dotfiles aren't ignored by default in FlatConfig,
       'packages/**/dist/**',
       'packages/create-plugin/templates/**',
+      'docusaurus/website/.docusaurus',
+      'docusaurus/website/build',
     ],
   },
-  {
-    name: 'plugin-tools/defaults',
-    files: ['**/*.{ts,tsx,js}'],
-  },
-  ...grafanaConfig,
+  // Focus the source code configs on source code so mdx codeblocks don't inherit all the eslint config
+  // which would make it very difficult to "incomplete" write snippets of code in the docs.
+  ...grafanaConfig.map((config) => ({
+    ...config,
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    ignores: ['docusaurus/docs/**'],
+  })),
   {
     name: 'plugin-e2e/overrides',
     rules: {
@@ -44,6 +50,33 @@ module.exports = [
           ],
         },
       ],
+    },
+  },
+  {
+    ...mdxPlugin.flat,
+    ...mdxPlugin.flatCodeBlocks,
+    name: 'website/mdx',
+    files: ['docusaurus/docs/**/*.{md,mdx}'],
+    languageOptions: {
+      parser: mdxParser,
+      parserOptions: {
+        extensions: ['.md', '.mdx'],
+        markdownExtensions: ['.md', '.mdx'],
+      },
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
+    },
+    processor: mdxPlugin.createRemarkProcessor({
+      lintCodeBlocks: true,
+    }),
+    rules: {
+      ...mdxPlugin.flat.rules,
+      ...mdxPlugin.flatCodeBlocks.rules,
     },
   },
 ];
