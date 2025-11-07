ef48c8a7d9af886abda28a07223f2f1c730e410e
Jack Westbrook <jack.westbrook@gmail.com>
2025-11-07 16:51:03 +0100

    feat(create-plugin): add a migration to move react deps from >=18 <19 to 18.3.x

---
+++ packages/create-plugin/src/migrations/scripts/005-react-18-3.ts
@@ -0,0 +1,19 @@
+import { Context } from '../context.js';
+import { addDependenciesToPackageJson, isVersionGreater } from '../utils.js';
+
+export default function migrate(context: Context) {
+  if (context.doesFileExist('package.json')) {
+    const packageJson = JSON.parse(context.getFile('package.json') || '{}');
+    if (packageJson.dependencies?.react) {
+      if (isVersionGreater(packageJson.dependencies.react, '18.0.0', true)) {
+        addDependenciesToPackageJson(context, { react: '^18.3.1' }, { '@types/react': '^18.3.1' });
+      }
+    }
+    if (packageJson.dependencies?.['react-dom']) {
+      if (isVersionGreater(packageJson.dependencies['react-dom'], '18.0.0', true)) {
+        addDependenciesToPackageJson(context, { 'react-dom': '^18.3.1' }, { '@types/react-dom': '^18.3.1' });
+      }
+    }
+  }
+  return context;
+}
