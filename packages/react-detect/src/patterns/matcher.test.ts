41f3e64ca76a475b057deabf668c14b8d7d757b2
Jack Westbrook <jack.westbrook@gmail.com>
2025-11-25 16:20:58 +0100

    test(react-detect): add a test case for bundled code

--- packages/react-detect/src/patterns/matcher.test.ts
+++ packages/react-detect/src/patterns/matcher.test.ts
@@ -1,19 +1,31 @@
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
 import { findDefaultProps } from './matcher.js';
 import { parseFile } from '../parser.js';
 
 describe('findDefaultProps', () => {
-  it('should find defaultProps assignments', () => {
+  it('should find defaultProps assignments in source code', () => {
     const code = `
       function MyComponent() {}
       MyComponent.defaultProps = { foo: 'bar' };
     `;
     const ast = parseFile(code, 'module.js');
-
-    console.log();
     const matches = findDefaultProps(ast, code, 'module.js');
 
     expect(matches).toHaveLength(1);
     expect(matches[0].pattern).toBe('defaultProps');
     expect(matches[0].line).toBe(3);
+  });
+
+  it('should find defaultProps in bundled code', () => {
+    const fixturePath = join(__dirname, '../../test/fixtures/patterns/module.defaultProps.js');
+    const code = readFileSync(fixturePath, 'utf-8');
+    const ast = parseFile(code, fixturePath);
+    const matches = findDefaultProps(ast, code, fixturePath);
+
+    expect(matches).toHaveLength(1);
+    expect(matches[0].pattern).toBe('defaultProps');
+    expect(matches[0].line).toBe(42);
+    expect(matches[0].matched).toContain('defaultProps');
   });
 });
---
+++ packages/react-detect/test/fixtures/patterns/module.defaultProps.js
@@ -0,0 +1,46 @@
+/* eslint-disable */
+define(['react'], (e) =>
+  (() => {
+    'use strict';
+    let r = {
+        959: (r) => {
+          r.exports = e;
+        },
+      },
+      t = {};
+    function o(e) {
+      let n = t[e];
+      if (void 0 !== n) {
+        return n.exports;
+      }
+      let a = (t[e] = { exports: {} });
+      return (r[e](a, a.exports, o), a.exports);
+    }
+    ((o.n = (e) => {
+      let r = e && e.__esModule ? () => e.default : () => e;
+      return (o.d(r, { a: r }), r);
+    }),
+      (o.d = (e, r) => {
+        for (let t in r) {
+          o.o(r, t) && !o.o(e, t) && Object.defineProperty(e, t, { enumerable: !0, get: r[t] });
+        }
+      }),
+      (o.o = (e, r) => Object.prototype.hasOwnProperty.call(e, r)),
+      (o.r = (e) => {
+        ('undefined' !== typeof Symbol &&
+          Symbol.toStringTag &&
+          Object.defineProperty(e, Symbol.toStringTag, { value: 'Module' }),
+          Object.defineProperty(e, '__esModule', { value: !0 }));
+      }));
+    let n = {};
+    (o.r(n), o.d(n, { default: () => u }));
+    let a = o(959),
+      l = o.n(a);
+    function d({ name: e }) {
+      return l().createElement('div', null, 'Hello, ', e, '!');
+    }
+    ((d.displayName = 'MyComponent'), (d.defaultProps = { name: 'World' }));
+    const u = d;
+    return n;
+  })());
+//# sourceMappingURL=module.js.map
