266ba1a59850ce1e461436250691c88288ec0253
Jack Westbrook <jack.westbrook@gmail.com>
2025-12-04 15:07:06 +0100

    Wip

--- packages/react-detect/src/file-scanner.ts
+++ packages/react-detect/src/file-scanner.ts
@@ -1,23 +1,6 @@
 import fg from 'fast-glob';
 import { join } from 'node:path';
 
-export async function findBundledJSfiles(directory: string): Promise<string[]> {
-  const distDirectory = join(directory, 'dist');
-
-  try {
-    const files = await fg('**/*.js', {
-      cwd: distDirectory,
-      absolute: true,
-      ignore: ['**/node_modules/**'],
-    });
-    return files;
-  } catch (error) {
-    throw new Error(
-      `Error finding JS files in ${distDirectory}: ${error instanceof Error ? error.message : 'Unknown error'}`
-    );
-  }
-}
-
 export async function findSourceMapFiles(directory: string): Promise<string[]> {
   const distDirectory = join(directory, 'dist');
 
