b0981277ec981917253aadb1de73cacec98fa821
Erik Sundell <erik.sundell87@gmail.com>
2025-12-16 10:19:30 +0100

    remove example script

--- packages/create-plugin/src/codemods/additions/additions.ts
+++ packages/create-plugin/src/codemods/additions/additions.ts
@@ -2,11 +2,6 @@ import { Codemod } from '../types.js';
 
 export default [
   {
-    name: 'example-addition',
-    description: 'Adds an example addition to the plugin',
-    scriptPath: import.meta.resolve('./scripts/example-addition.js'),
-  },
-  {
     name: 'i18n',
     description: 'Adds internationalization (i18n) support to the plugin',
     scriptPath: import.meta.resolve('./scripts/i18n/index.js'),
