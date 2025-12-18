86732379f65c74b0552f60d6b4500cc1f67f3c5d
Erik Sundell <erik.sundell87@gmail.com>
2025-12-18 08:04:21 +0100

    add bundle grafana ui addition

--- packages/create-plugin/src/codemods/additions/additions.ts
+++ packages/create-plugin/src/codemods/additions/additions.ts
@@ -6,4 +6,9 @@ export default [
     description: 'Adds an example addition to the plugin',
     scriptPath: import.meta.resolve('./scripts/example-addition.js'),
   },
+  {
+    name: 'bundle-grafana-ui',
+    description: 'Configures the plugin to bundle @grafana/ui instead of using the external provided by Grafana',
+    scriptPath: import.meta.resolve('./scripts/bundle-grafana-ui/index.js'),
+  },
 ] satisfies Codemod[];
