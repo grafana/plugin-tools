ab558d81dae50c2ec140a4781a9e6da3c14a43ac
Jack Westbrook <jack.westbrook@gmail.com>
2026-01-07 16:22:03 +0100

    fix(create-plugin): stop copying across all images to prevent bundle size bloat

--- packages/create-plugin/templates/common/.config/rspack/rspack.config.ts
+++ packages/create-plugin/templates/common/.config/rspack/rspack.config.ts
@@ -161,14 +161,10 @@ const config = async (env): Promise<Configuration> => {
           { from: 'plugin.json', to: '.' },
           { from: '../LICENSE', to: '.' },
           { from: '../CHANGELOG.md', to: '.', force: true },
-          { from: '**/*.json', to: '.' }, // TODO<Add an error for checking the basic structure of the repo>
-          { from: '**/*.svg', to: '.', noErrorOnMissing: true }, // Optional
-          { from: '**/*.png', to: '.', noErrorOnMissing: true }, // Optional
-          { from: '**/*.html', to: '.', noErrorOnMissing: true }, // Optional
-          { from: 'img/**/*', to: '.', noErrorOnMissing: true }, // Optional
-          { from: 'libs/**/*', to: '.', noErrorOnMissing: true }, // Optional
-          { from: 'static/**/*', to: '.', noErrorOnMissing: true }, // Optional
-          { from: '**/query_help.md', to: '.', noErrorOnMissing: true }, // Optional
+          { from: '**/*.json', to: '.' },
+          { from: 'img/logo.svg', to: '.', noErrorOnMissing: true },
+          { from: 'img/screenshots/**/*', to: '.', noErrorOnMissing: true },
+          { from: '**/query_help.md', to: '.', noErrorOnMissing: true },
         ],
       }),
       // Replace certain template-variables in the README and plugin.json
