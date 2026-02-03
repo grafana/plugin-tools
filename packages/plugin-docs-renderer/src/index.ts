63d7ddbfb898f72d51def7e3549029294a7c3658
Erik Sundell <erik.sundell87@gmail.com>
2026-02-03 11:00:47 +0100

    add basic skeleton for new package

---
+++ packages/plugin-docs-renderer/src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @grafana/plugin-docs-renderer
+ *
+ * A library for rendering Grafana plugin documentation from markdown files.
+ */
+
+// Export types
+export type { Manifest, Page, MarkdownFiles } from './types.js';
