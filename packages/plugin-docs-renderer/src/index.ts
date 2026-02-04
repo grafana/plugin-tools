9d7a3512084937f55de9f46fc6580f4377b1791a
Erik Sundell <erik.sundell87@gmail.com>
2026-02-04 09:46:20 +0100

    add very basic dev server

--- packages/plugin-docs-renderer/src/index.ts
+++ packages/plugin-docs-renderer/src/index.ts
@@ -8,3 +8,7 @@ export type { ParsedMarkdown } from './parser.js';
 // export loader functions
 export { loadDocsFolder } from './loader.js';
 export type { LoadedDocs } from './loader.js';
+
+// export server functions
+export { startServer } from './server.js';
+export type { ServerOptions } from './server.js';
