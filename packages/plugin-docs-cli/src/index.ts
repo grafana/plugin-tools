3691b0f4a875a4e988272c9c5e4c25b461fc9a02
Erik Sundell <erik.sundell87@gmail.com>
2026-02-09 16:35:13 +0100

    initial commit - add new docs-cli package

---
+++ packages/plugin-docs-cli/src/index.ts
@@ -0,0 +1,6 @@
+// This package is a CLI tool, not a library
+// No exports - use the binary instead:
+// npx @grafana/plugin-docs-cli serve ./docs
+
+// If you need programmatic access to parsing, use:
+// import { parseMarkdown } from '@grafana/plugin-docs-renderer';
