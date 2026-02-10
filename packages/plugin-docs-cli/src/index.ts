d61018e6baa59e315b15c3da695da0f4909d90ac
Erik Sundell <erik.sundell87@gmail.com>
2026-02-10 07:55:00 +0100

    remove unnecessary main and types fields from package.json

--- packages/plugin-docs-cli/src/index.ts
+++
@@ -1,6 +0,0 @@
-// This package is a CLI tool, not a library
-// No exports - use the binary instead:
-// npx @grafana/plugin-docs-cli serve ./docs
-
-// If you need programmatic access to parsing, use:
-// import { parseMarkdown } from '@grafana/plugin-docs-renderer';
