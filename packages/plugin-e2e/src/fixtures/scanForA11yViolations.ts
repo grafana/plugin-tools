5c6f66da5af6e54ebe7472cd777ed47d7a5b2f7f
Paul Marbach <paul.marbach@grafana.com>
2026-02-18 09:42:00 -0500

    use attachment instead of annotation

--- packages/plugin-e2e/src/fixtures/scanForA11yViolations.ts
+++ packages/plugin-e2e/src/fixtures/scanForA11yViolations.ts
@@ -4,26 +4,23 @@ import { AxeResults, RunOptions as AxeRunOptions } from 'axe-core';
 
 import { PlaywrightArgs } from '../types';
 
+export const DEFAULT_A11Y_TAGS = ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa' /* 'best-practice' */];
+
 export const scanForA11yViolations: TestFixture<
   (options?: AxeRunOptions) => Promise<AxeResults>,
   PlaywrightArgs
 > = async ({ page }, use, testInfo) => {
   await use(async (options?: AxeRunOptions) => {
-    const builder = new AxeBuilder({ page }).withTags([
-      'wcag2a',
-      'wcag2aa',
-      'wcag21a',
-      'wcag21aa', /* 'best-practice' */
-    ]);
+    const builder = new AxeBuilder({ page }).withTags(DEFAULT_A11Y_TAGS);
     if (options) {
       builder.options(options);
     }
 
     const accessibilityScanResults = await builder.analyze();
 
-    testInfo.annotations.push({
-      type: 'axe-a11y',
-      description: JSON.stringify(accessibilityScanResults, null, 2),
+    testInfo.attach('axe', {
+      body: JSON.stringify(accessibilityScanResults, null, 2),
+      contentType: 'application/json',
     });
 
     return accessibilityScanResults;
