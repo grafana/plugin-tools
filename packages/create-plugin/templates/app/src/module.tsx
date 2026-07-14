4631975a09ea3a3b2c2e4ab3d38983701b0796a8
joshhunt <josh.hunt@grafana.com>
2026-07-14 09:43:16 +0100

    Remove suspense boundary from App plugins

--- packages/create-plugin/templates/app/src/module.tsx
+++ packages/create-plugin/templates/app/src/module.tsx
@@ -1,26 +1,12 @@
-import React, { Suspense, lazy } from 'react';
-import { AppPlugin, type AppRootProps } from '@grafana/data';
-import { LoadingPlaceholder } from '@grafana/ui';
-import type { AppConfigProps } from './components/AppConfig/AppConfig';
+import { lazy } from 'react';
+import { AppPlugin } from '@grafana/data';
 
 const LazyApp = lazy(() => import('./components/App/App'));
 const LazyAppConfig = lazy(() => import('./components/AppConfig/AppConfig'));
 
-const App = (props: AppRootProps) => (
-  <Suspense fallback={<LoadingPlaceholder text="" />}>
-    <LazyApp {...props} />
-  </Suspense>
-);
-
-const AppConfig = (props: AppConfigProps) => (
-  <Suspense fallback={<LoadingPlaceholder text="" />}>
-    <LazyAppConfig {...props} />
-  </Suspense>
-);
-
-export const plugin = new AppPlugin<{}>().setRootPage(App).addConfigPage({
+export const plugin = new AppPlugin<{}>().setRootPage(LazyApp).addConfigPage({
   title: 'Configuration',
   icon: 'cog',
-  body: AppConfig,
+  body: LazyAppConfig,
   id: 'configuration',
 });
