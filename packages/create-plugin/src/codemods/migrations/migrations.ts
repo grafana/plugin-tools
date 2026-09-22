022e3487c0bdd7e8f5551718de0ff8217bddf0b5
L2D2Grafana <liza.detrick@grafana.com>
2026-09-22 08:27:10 -0700

    fix(create-plugin): update version to 7.11.1

--- packages/create-plugin/src/codemods/migrations/migrations.ts
+++ packages/create-plugin/src/codemods/migrations/migrations.ts
@@ -93,7 +93,7 @@ export default [
   },
   {
     name: '014-eslint-config-v10',
-    version: '7.10.2',
+    version: '7.11.1',
     description:
       'Update @grafana/eslint-config to v10: drop the removed ./flat.js subpath import and swap @stylistic/eslint-plugin-ts for @stylistic/eslint-plugin.',
     scriptPath: import.meta.resolve('./scripts/014-eslint-config-v10.js'),
